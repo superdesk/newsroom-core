805fe0f1b7a009962627185016138afab099c19d
olegpshenichniy <olegpshenichniy@gmail.com>
2021-01-20 17:16:33 +0100

    Reusable pytest fixtures.

--- tests/news_api/conftest.py
+++ tests/news_api/conftest.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 from pytest import fixture
-from tests.conftest import update_config # noqa
+from newsroom.tests.conftest import update_config # noqa
 
 root = (Path(__file__).parent / '..').resolve()
 sys.path.insert(0, str(root))
