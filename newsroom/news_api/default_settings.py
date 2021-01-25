51179bf45905a7f7b96e71f87b452dc214675f84
olegpshenichniy <olegpshenichniy@gmail.com>
2021-01-25 16:51:00 +0100

    Add missed default configs for newsapi.

--- newsroom/news_api/default_settings.py
+++ newsroom/news_api/default_settings.py
@@ -34,6 +34,9 @@ CORE_APPS = [
 
 INSTALLED_APPS = []
 
+LANGUAGES = ['en', 'fi', 'cs', 'fr_CA']
+DEFAULT_LANGUAGE = 'en'
+
 # newsroom default db and index names
 MONGO_DBNAME = env('MONGO_DBNAME', 'newsroom')
 # mongo
