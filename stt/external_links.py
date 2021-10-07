c98c297ed12bf3b8fa139d7a0e364885d4a837d9
Petr Jasek <jasekpetr@gmail.com>
2021-10-07 12:15:03 +0200

    remove stt specific code

--- stt/external_links.py
+++
@@ -1,5 +0,0 @@
-
-def init_app(app):
-    app.sidenav('STT Pro', url='https://pro.stt.fi', icon='calendar', group=8)
-    app.sidenav('Lehtikuvan Kuvakauppa', url='http://kuvakauppa.lehtikuva.fi', icon='photo', group=8)
-    app.sidenav('STT Info -tiedotejakelu', url='https://www.sttinfo.fi', icon='info', group=8)
