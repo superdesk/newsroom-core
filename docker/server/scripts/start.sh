1081489c41a58f8b460c1ff479066eb9320d1a5d
Petr Jasek <jasekpetr@gmail.com>
2021-11-04 11:18:51 +0100

    add docker setup for local ui development

---
+++ docker/server/scripts/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# set -e
+
+cd /opt/newsroom/
+
+# wait for elastic to be up
+printf 'waiting for elastic.'
+until $(curl --output /dev/null --silent --fail "${ELASTICSEARCH_URL}"); do
+    printf '.'
+    sleep .5
+done
+echo 'done.'
+
+# wait for client 
+printf 'waiting for client.'
+until $(curl --output /dev/null --silent --fail "${WEBPACK_SERVER_URL}/manifest.json"); do
+    printf '.'
+    sleep .5
+done
+echo 'done.'
+
+# app init
+python3 manage.py create_user admin@localhost.com admin admin admin true
+python3 manage.py elastic_init
+
+if [[ -d dump ]]; then
+    echo 'installing demo data'
+    mongorestore -h mongo --gzip dump
+    python3 manage.py index_from_mongo --all
+fi
+
+exec "$@"
