5c719ce5db55353917f7875f928007d39104aab0
Mark Pittaway <mark.pittaway@gmail.com>
2021-11-25 16:50:31 +1100

    [NHUB-27] Change embargo label colour when it expires

--- assets/wire/components/fields/Embargo.jsx
+++ assets/wire/components/fields/Embargo.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {gettext, fullDate, getEmbargo} from 'utils';
+import moment from 'moment-timezone';
 import classNames from 'classnames';
+
+import {gettext, fullDate, getEmbargo} from 'utils';
 
 export class Embargo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {embargo: getEmbargo(props.item)};
+        this.state = {
+            embargo: getEmbargo(props.item),
+            forceRender: false,
+        };
     }
 
     componentDidMount() {
-        if (this.elem && this.state.embargo) {
-            $(this.elem).tooltip({
-                placement: 'bottom',
-                title: fullDate(this.state.embargo),
-            });
+        if (this.state.embargo) {
+            if (this.elem) {
+                $(this.elem).tooltip({
+                    placement: 'bottom',
+                    title: fullDate(this.state.embargo),
+                });
+            }
+
+            // Calculate the duration for when the embargo is lifted
+            const embargo = moment(this.props.item.embargoed);
+            const embargoLiftsIn = moment.duration(embargo.diff(moment()));
+
+            // Make sure we aren't setting a timeout that is too big
+            if (embargoLiftsIn.asHours() < 48) {
+                setTimeout(() => {
+                    this.setState({
+                        embargo: getEmbargo(this.props.item),
+                        forceRender: true,
+                    });
+                }, embargoLiftsIn.asMilliseconds());
+            }
         }
     }
 
@@ -25,14 +46,18 @@ export class Embargo extends React.Component {
     }
 
     render() {
-        if (!this.state.embargo) {
+        if (!this.state.embargo && !this.state.forceRender) {
             return null;
         }
 
         return (
             <span
                 ref={(elem) => this.elem = elem}
-                className={classNames('label label--red ', {'ml-4': !this.props.isCard})}
+                className={classNames('label', {
+                    'ml-4': !this.props.isCard,
+                    'label--red': !this.state.forceRender,
+                    'label--available': this.state.forceRender,
+                })}
             >{gettext('embargo')}</span>
         );
     }
