18d417a9699f11db1f8624d90b0a3f764642d8d4
dev-tsoni <tanuj.soni@sourcefabric.org>
2022-02-18 17:49:55 +0530

    NHUB-91 FIX: Fixed the test case error.

--- assets/components/TextInput.jsx
+++ assets/components/TextInput.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {gettext} from 'utils';
+
 import InputWrapper from './InputWrapper';
+
+import {gettext} from 'utils';
 
 function TextInput({
     type,
