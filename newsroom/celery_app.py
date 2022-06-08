07d5189d349f0702e942bb8391a62810a33433d0
Petr Jašek <jasekpetr@gmail.com>
2022-06-08 11:03:10 +0200

    fix celery config (#168)
    - set global time limit
    - log when there is an error/timeout
    
    NHUB-151

--- newsroom/celery_app.py
+++ newsroom/celery_app.py
@@ -17,14 +17,13 @@ Created on May 29, 2014
 import logging
 
 import arrow
-import werkzeug
 from bson import ObjectId
 from celery import Celery
+from celery.worker.request import Request
 from kombu.serialization import register
 from eve.io.mongo import MongoJSONEncoder
 from eve.utils import str_to_date
 from flask import json
-from superdesk.errors import SuperdeskError
 from superdesk.celery_app import (  # noqa
     finish_subtask_from_progress,
     finish_task_for_progress,
@@ -86,24 +85,47 @@ def handle_exception(exc):
     logger.exception(exc)
 
 
+class NewsroomRequest(Request):
+    """
+    Based on https://docs.celeryq.dev/en/stable/userguide/tasks.html#requests-and-custom-requests
+    """
+
+    def on_timeout(self, soft, timeout):
+        super().on_timeout(soft, timeout)
+        if not soft:
+            logger.warning(
+                'A hard timeout was enforced for task %s',
+                self.task.name
+            )
+
+    def on_failure(self, exc_info, send_failed_event=True, return_ok=False):
+        super().on_failure(
+            exc_info,
+            send_failed_event=send_failed_event,
+            return_ok=return_ok
+        )
+        logger.warning(
+            'Failure detected for task %s',
+            self.task.name
+        )
+
+
 class AppContextTask(TaskBase):  # type: ignore
     abstract = True
     serializer = 'newsroom/json'
-    app_errors = (
-        SuperdeskError,
-        werkzeug.exceptions.InternalServerError,  # mongo layer err
-    )
+    Request = NewsroomRequest
 
     def __call__(self, *args, **kwargs):
         with newsroom.flask_app.app_context():
             try:
                 return super().__call__(*args, **kwargs)
-            except self.app_errors as e:
+            except Exception as e:
+                logger.warning('Error when calling task %s', self.task.name)
                 handle_exception(e)
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):
-        with newsroom.flask_app.app_context():
-            handle_exception(exc)
+        logger.warning('Failure detected for task %s', self.task.name)
+        handle_exception(exc)
 
 
 celery.Task = AppContextTask
