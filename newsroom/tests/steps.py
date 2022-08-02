3461d24cfa4b3761f02d96b7f026e387da1ff5d2
Mark Pittaway <mark.pittaway@gmail.com>
2022-08-02 17:19:03 +1000

    fix: flake8 errors

--- newsroom/tests/steps.py
+++ newsroom/tests/steps.py
@@ -49,10 +49,10 @@ def we_get_text_in_response(context, text):
 @then('we "{get}" "{text}" in atom xml response')
 def we_get_text_in_atom_xml_response(context, get, text):
     with context.app.test_request_context(context.app.config['URL_PREFIX']):
-        assert(isinstance(get_body(context.response), str))
+        assert isinstance(get_body(context.response), str)
         tree = lxml.etree.fromstring(get_body(context.response).encode('utf-8'))
         assert '{http://www.w3.org/2005/Atom}feed' == tree.tag
         if get == 'get':
-            assert(text in get_body(context.response))
+            assert text in get_body(context.response)
         else:
             assert (text not in get_body(context.response))
