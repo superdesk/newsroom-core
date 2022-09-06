08d657d064a2979aa7c94366d42e9a3cdc0fa8f0
Mark Pittaway <mark.pittaway@gmail.com>
2022-09-06 17:55:23 +1000

    Add tests

--- tests/core/test_search_config.py
+++ tests/core/test_search_config.py
@@ -2,7 +2,8 @@ from flask import json
 from flask.testing import FlaskClient
 
 from newsroom.factory.app import BaseNewsroomApp
-from newsroom.agenda.agenda import AgendaResource, aggregations
+from newsroom.agenda.agenda import AgendaResource, aggregations as agenda_aggregations
+from newsroom.wire.search import WireSearchResource, get_aggregations as get_wire_aggregations
 from newsroom.search_config import init_nested_aggregation
 from newsroom.tests.conftest import reset_elastic
 
@@ -47,6 +48,36 @@ test_event_2 = {
     }
 }
 
+test_wire_item_1 = {
+    "guid": "foo1",
+    "type": "text",
+    "headline": "Foo",
+    "firstcreated": "2017-11-27T08:00:57+0000",
+    "body_html": "<p>foo bar</p>",
+    "genre": [{"name": "News", "code": "news"}],
+    "subject": [{
+        "qcode": "1523",
+        "name": "Test Subject",
+    }],
+}
+
+test_wire_item_2 = {
+    "guid": "foo2",
+    "type": "text",
+    "headline": "Foo",
+    "firstcreated": "2017-11-27T08:00:57+0000",
+    "body_html": "<p>foo bar</p>",
+    "genre": [{"name": "News", "code": "news"}],
+    "subject": [{
+        "qcode": "1523",
+        "name": "Test Subject",
+    }, {
+        "qcode": "abcd",
+        "name": "Sporting Event",
+        "scheme": "distribution"
+    }],
+}
+
 
 def test_default_agenda_groups_config(app: BaseNewsroomApp, client: FlaskClient):
     """Tests the default config (disabled nested search groups)"""
@@ -62,9 +93,9 @@ def test_default_agenda_groups_config(app: BaseNewsroomApp, client: FlaskClient)
     assert "urgency" in group_fields
     assert "place" in group_fields
 
-    assert aggregations["subject"] == {"terms": {"field": "subject.name", "size": 20}}
+    assert agenda_aggregations["subject"] == {"terms": {"field": "subject.name", "size": 20}}
 
-    # Test search aggregations
+    # Test search agenda_aggregations
     client.post("/push", data=json.dumps(test_event_1), content_type="application/json")
     resp = client.get("/agenda/search")
     data = json.loads(resp.get_data())
@@ -93,15 +124,15 @@ def test_custom_agenda_groups_config(app: BaseNewsroomApp, client: FlaskClient):
     init_nested_aggregation(
         AgendaResource,
         app.config["AGENDA_GROUPS"],
-        aggregations
+        agenda_aggregations
     )
     reset_elastic(app)
 
-    # Test if the Eve & aggregations config has been updated
+    # Test if the Eve & agenda_aggregations config has been updated
 
     # Test generated/modified aggregation configs
     # Parent field
-    assert aggregations["subject"] == {
+    assert agenda_aggregations["subject"] == {
         "nested": {"path": "subject"},
         "aggs": {
             "subject_filtered": {
@@ -112,7 +143,7 @@ def test_custom_agenda_groups_config(app: BaseNewsroomApp, client: FlaskClient):
     }
 
     # Nested Field
-    assert aggregations["event_type"] == {
+    assert agenda_aggregations["event_type"] == {
         "nested": {"path": "subject"},
         "aggs": {
             "event_type_filtered": {
@@ -122,7 +153,7 @@ def test_custom_agenda_groups_config(app: BaseNewsroomApp, client: FlaskClient):
         },
     }
 
-    # Test search aggregations
+    # Test search agenda_aggregations
     client.post("/push", data=json.dumps(test_event_1), content_type="application/json")
     client.post("/push", data=json.dumps(test_event_2), content_type="application/json")
     resp = client.get("/agenda/search")
@@ -161,3 +192,118 @@ def test_custom_agenda_groups_config(app: BaseNewsroomApp, client: FlaskClient):
     data = json.loads(resp.get_data())
     assert len(data["_items"]) == 1
     assert data["_items"][0]["_id"] == "event2"
+
+
+def test_default_wire_groups_config(app: BaseNewsroomApp, client: FlaskClient):
+    """Tests the default wire config (disabled nested search groups)"""
+
+    assert len(app.config["WIRE_GROUPS"]) == 5
+
+    group_fields = [
+        group.get("field")
+        for group in app.config["WIRE_GROUPS"]
+    ]
+    assert "service" in group_fields
+    assert "subject" in group_fields
+    assert "genre" in group_fields
+    assert "urgency" in group_fields
+    assert "place" in group_fields
+
+    wire_aggregations = get_wire_aggregations()
+    assert wire_aggregations["subject"] == {"terms": {"field": "subject.name", "size": 20}}
+
+    client.post("/push", data=json.dumps(test_wire_item_1), content_type="application/json")
+    res = client.get("/wire/search")
+    data = json.loads(res.get_data())
+    assert data["_aggregations"]["subject"] == {
+        "doc_count_error_upper_bound": 0,
+        "sum_other_doc_count": 0,
+        "buckets": [{
+            "doc_count": 1,
+            "key": "Test Subject",
+        }]
+    }
+
+
+def test_custom_wire_groups_config(app: BaseNewsroomApp, client: FlaskClient):
+    """Tests custom wire config, enabling nested search groups"""
+
+    app.config["WIRE_GROUPS"].append({
+        "field": "distribution",
+        "label": "Distribution",
+        "nested": {
+            "parent": "subject",
+            "field": "scheme",
+            "value": "distribution"
+        },
+    })
+    wire_aggregations = get_wire_aggregations()
+    init_nested_aggregation(
+        WireSearchResource,
+        app.config["WIRE_GROUPS"],
+        wire_aggregations
+    )
+    reset_elastic(app)
+
+    # Test generated/modified aggregation configs
+    # Parent field
+    assert wire_aggregations["subject"] == {
+        "nested": {"path": "subject"},
+        "aggs": {
+            "subject_filtered": {
+                "filter": {"bool": {"must_not": [{"terms": {"subject.scheme": ["distribution"]}}]}},
+                "aggs": {"subject": {"terms": {"field": "subject.name", "size": 20}}},
+            },
+        },
+    }
+
+    # Nested Field
+    assert wire_aggregations["distribution"] == {
+        "nested": {"path": "subject"},
+        "aggs": {
+            "distribution_filtered": {
+                "filter": {"bool": {"must": [{"term": {"subject.scheme": "distribution"}}]}},
+                "aggs": {"distribution": {"terms": {"field": "subject.name", "size": 20}}},
+            },
+        },
+    }
+
+    # Test search wire_aggregations
+    client.post("/push", data=json.dumps(test_wire_item_1), content_type="application/json")
+    client.post("/push", data=json.dumps(test_wire_item_2), content_type="application/json")
+    resp = client.get("/wire/search")
+    data = json.loads(resp.get_data())
+    assert data["_aggregations"]["subject"] == {
+        "doc_count": 3,
+        "subject_filtered": {
+            "doc_count": 2,
+            "subject": {
+                "doc_count_error_upper_bound": 0,
+                "sum_other_doc_count": 0,
+                "buckets": [{
+                    "doc_count": 2,
+                    "key": "Test Subject",
+                }],
+            },
+        },
+    }
+    assert data["_aggregations"]["distribution"] == {
+        "doc_count": 3,
+        "distribution_filtered": {
+            "doc_count": 1,
+            "distribution": {
+                "doc_count_error_upper_bound": 0,
+                "sum_other_doc_count": 0,
+                "buckets": [{
+                    "doc_count": 1,
+                    "key": "Sporting Event",
+                }],
+            },
+        },
+    }
+
+    # Search using the new search group, ``distribution==Sporting Event``
+    resp = client.get("/wire/search?filter=%7B%22distribution%22%3A%5B%22Sporting%20Event%22%5D%7D")
+    data = json.loads(resp.get_data())
+    assert len(data["_items"]) == 1
+    assert data["_items"][0]["_id"] == "foo2"
