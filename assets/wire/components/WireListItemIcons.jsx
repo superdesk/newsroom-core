ccd765fbd701a2d436f16d90496e120788eb88dc
Darko Tomic <darko.tomic@gmail.com>
2022-09-14 12:03:59 +0200

    NHUB-164

--- assets/wire/components/WireListItemIcons.jsx
+++ assets/wire/components/WireListItemIcons.jsx
@@ -7,22 +7,22 @@ function WireListItemIcons({item, picture, divider}) {
         <div className='wire-articles__item__icons'>
             {item.type === 'text' &&
                 <span className='wire-articles__item__icon'>
-                    <i className='icon--text icon--gray'></i>
+                    <i className='icon--text icon--gray-dark'></i>
                 </span>
             }
             {picture &&
                 <span className='wire-articles__item__icon'>
-                    <i className='icon--photo icon--gray'></i>
+                    <i className='icon--photo icon--gray-dark'></i>
                 </span>
             }
             {hasVideo(item) &&
                 <span className='wire-articles__item__icon'>
-                    <i className='icon--video icon--gray'></i>
+                    <i className='icon--video icon--gray-dark'></i>
                 </span>
             }
             {hasAudio(item) &&
                 <span className='wire-articles__item__icon'>
-                    <i className='icon--audio icon--gray'></i>
+                    <i className='icon--audio icon--gray-dark'></i>
                 </span>
             }
             {divider &&
