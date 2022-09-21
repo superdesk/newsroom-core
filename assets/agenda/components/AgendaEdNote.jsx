d09688d20349c9355d1ae3efa1229fab82a6bd45
Petr Jašek <jasekpetr@gmail.com>
2022-09-21 10:15:43 +0200

    fix agenda ednote english strings from sd (#209)
    CPNHUB-79

--- assets/agenda/components/AgendaEdNote.jsx
+++ assets/agenda/components/AgendaEdNote.jsx
@@ -8,14 +8,42 @@ import {gettext} from 'utils';
 export default function AgendaEdNote({item, plan, secondaryNoteField, noMargin}) {
     // We display Secondary Note only from 'item' for now
     const displaySecondaryNote = secondaryNoteField && get(item, secondaryNoteField);
+
     if (!item.ednote && !plan.ednote && !displaySecondaryNote) {
         return null;
     }
 
     const tooltip = !item.ednote && !plan.ednote ? gettext('Reason') : gettext('Editorial Note');
-    const getMultiLineNote = (note) => (note && note.split('\n').map((t, key) =>
+    const fixText = (text) => {
+        // remove prefixes added by planning
+        // which are always in english
+        const POSTPONED = 0;
+        const RESCHEDULED = 1;
+        const prefixes = [];
+
+        prefixes[POSTPONED] = 'Event Postponed: ';
+        prefixes[RESCHEDULED] = 'Event Rescheduled: ';
+
+        return prefixes.reduce((_text, prefix, index) => {
+            if (_text.startsWith(prefix)) {
+                let reason = _text.substr(prefix.length);
+
+                switch (index) {
+                case POSTPONED:
+                    return gettext('Event Postponed: {{reason}}', {reason});
+                case RESCHEDULED:
+                    return gettext('Event Rescheduled: {{reason}}', {reason});
+                }
+            }
+
+            return _text;
+        }, text.trim());
+    };
+
+    const getMultiLineNote = (note) => (note && fixText(note).split('\n').map((t, key) =>
         <span key={key}>{t}<br/></span>)
     );
+
     const getNoteFields = () => {
         if (!item.ednote && !plan.ednote) {
             // Display only Secondary Note
