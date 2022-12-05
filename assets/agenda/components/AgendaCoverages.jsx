07b0a43ba398ac661374300d8843b6469e08cefb
Mark Pittaway <mark.pittaway@gmail.com>
2022-12-05 17:17:45 +1100

    [NHUB-200] ui: Improve coverage preview when info restricted Also fix an issue with coverage info leaking from Wire preview

--- assets/agenda/components/AgendaCoverages.jsx
+++ assets/agenda/components/AgendaCoverages.jsx
@@ -29,47 +29,78 @@ function AgendaCoveragesComponent({item, coverages, wireItems, actions, user, on
         return slugline ? ` | ${slugline}` : '';
     };
 
-    return coverages.map((coverage) => (
-        <div className={classNames('coverage-item',
-            {'coverage-item--clickable': onClick})} key={coverage.coverage_id} onClick={onClick}
-        title={onClick ? gettext('Open Agenda in new tab') : onClick} >
-            <div className='coverage-item__row flex-column align-items-start'
-                title={getCoverageTooltip(coverage)}
-            >
-                <span className={classNames('coverage-item__coverage-icon', WORKFLOW_COLORS[coverage.workflow_status])}>                    
-                    <i className={`icon-small--coverage-${getCoverageIcon(coverage.coverage_type)} mr-2`}></i>
-                    <span>{`${getCoverageDisplayName(coverage.coverage_type)}${getSlugline(coverage)}`}</span>
-                </span>
-                {coverage.workflow_status !== WORKFLOW_STATUS.COMPLETED && coverage.scheduled != null && (
-                    <span className='d-flex text-nowrap'>
-                        <i className='icon-small--clock icon--gray-dark mr-1'></i>
-                        <span className='coverage-item__text-label mr-1'>{gettext('expected')}:</span>
-                        <span>{formatCoverageDate(coverage)}</span>
-                    </span>
-                )}
-            </div>
-            {coverage.coverage_provider && <div className='coverage-item__row'>
-                <span className='coverage-item__text-label mr-1'>{gettext('Source')}:</span>
-                <span className='mr-2'>{coverage.coverage_provider}</span>
-            </div>}
-            <CoverageItemStatus
-                coverage={coverage}
-                item={item}
-                wireItems={wireItems}
-                actions={actions}
-                user={user}
-                coverageData={getDataFromCoverages(item)}
-                hideViewContentItems={hideViewContentItems}
-                contentLinkTarget={contentLinkTarget}
-            />
-        </div>
-    ));
+    const coveragesWithoutState = coverages.filter((coverage) => WORKFLOW_COLORS[coverage.workflow_status] == null);
+    const coveragesWithState = coverages.filter((coverage) => WORKFLOW_COLORS[coverage.workflow_status] != null);
+
+    return (
+        <React.Fragment>
+            {!coveragesWithoutState.length ? null : (
+                <div>
+                    {coveragesWithoutState.map((coverage) => (
+                        <i
+                            className={`icon--coverage-${getCoverageIcon(coverage.coverage_type)} mr-2`}
+                            key={coverage.coverage_id}
+                            title={getCoverageTooltip(coverage)}
+                        />
+                    ))}
+                </div>
+            )}
+            {coveragesWithState.map((coverage) => (
+                <div
+                    className={classNames(
+                        'coverage-item',
+                        {'coverage-item--clickable': onClick}
+                    )}
+                    key={coverage.coverage_id}
+                    onClick={onClick}
+                    title={onClick ? gettext('Open Agenda in new tab') : onClick}
+                >
+                    <div
+                        className='coverage-item__row flex-column align-items-start'
+                        title={getCoverageTooltip(coverage)}
+                    >
+                        <span className={classNames('coverage-item__coverage-icon', WORKFLOW_COLORS[coverage.workflow_status])}>
+                            <i className={`icon-small--coverage-${getCoverageIcon(coverage.coverage_type)} mr-2`}></i>
+                            <span>{`${getCoverageDisplayName(coverage.coverage_type)}${getSlugline(coverage)}`}</span>
+                        </span>
+                        {coverage.workflow_status !== WORKFLOW_STATUS.COMPLETED && coverage.scheduled != null && (
+                            <span className='d-flex text-nowrap'>
+                                <i className='icon-small--clock icon--gray-dark mr-1'></i>
+                                <span className='coverage-item__text-label mr-1'>{gettext('expected')}:</span>
+                                <span>{formatCoverageDate(coverage)}</span>
+                            </span>
+                        )}
+                    </div>
+                    {coverage.coverage_provider && (
+                        <div className='coverage-item__row'>
+                            <span className='coverage-item__text-label mr-1'>{gettext('Source')}:</span>
+                            <span className='mr-2'>{coverage.coverage_provider}</span>
+                        </div>
+                    )}
+                    <CoverageItemStatus
+                        coverage={coverage}
+                        item={item}
+                        wireItems={wireItems}
+                        actions={actions}
+                        user={user}
+                        coverageData={getDataFromCoverages(item)}
+                        hideViewContentItems={hideViewContentItems}
+                        contentLinkTarget={contentLinkTarget}
+                    />
+                </div>
+            ))}
+        </React.Fragment>
+    );
 }
 
 AgendaCoveragesComponent.propTypes = {
     item: PropTypes.object,
     coverages: PropTypes.arrayOf(PropTypes.object),
     wireItems: PropTypes.array,
+    actions: PropTypes.array,
+    user: PropTypes.string,
+    onClick: PropTypes.func,
+    hideViewContentItems: PropTypes.bool,
     contentLinkTarget: PropTypes.string,
 };
 
