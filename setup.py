28898c7da83d5e37eceebe7cb3b826a8c7db593d
Mark Pittaway <mark.pittaway@gmail.com>
2023-01-10 20:47:29 +1100

    Release v2.3.0-rc5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(requirements_txt_path, "r") as r:
 
 setup(
     name="Newsroom-Core",
-    version="2.3.0-rc4",
+    version="2.3.0-rc5",
     description="Newsroom Core library",
     author="Sourcefabric",
     url="https://github.com/superdesk/newsroom-core",
