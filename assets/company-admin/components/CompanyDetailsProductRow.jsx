571ec902d2b386b8f4276aa01a0215efd578e104
Mark Pittaway <mark.pittaway@gmail.com>
2023-01-31 19:55:21 +1100

    UI: CompanyAdmin Detials page

---
+++ assets/company-admin/components/CompanyDetailsProductRow.jsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import {gettext} from 'utils';
+
+
+export function CompanyDetailsProductRow({seat, onNameClicked, showSeatRequestModal}) {
+    return (
+        <tr>
+            <td
+                onClick={onNameClicked}
+                className={!seat.max_reached ? undefined : 'text-danger'}
+            >
+                {seat.name}
+            </td>
+            <td className={!seat.max_reached ? undefined : 'text-danger'}>{seat.assigned_seats}/{seat.max_seats}</td>
+            <td className="font-light">{seat.description}</td>
+            <td>
+                <button
+                    className="btn btn-sm btn-outline-light"
+                    onClick={() => showSeatRequestModal([seat._id])}
+                >
+                    {gettext('Request more seats')}
+                </button>
+            </td>
+        </tr>
+    )
+}
+
+CompanyDetailsProductRow.propTypes = {
+    seat: PropTypes.shape({
+        _id: PropTypes.string,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        section: PropTypes.string,
+        max_seats: PropTypes.number,
+        assigned_seats: PropTypes.number,
+        max_reached: PropTypes.bool,
+    }),
+    onNameClicked: PropTypes.func,
+    showSeatRequestModal: PropTypes.func,
+}
