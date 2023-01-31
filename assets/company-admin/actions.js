75fa59b5e6f5ba2e8abd70eba6b563d65a550384
Mark Pittaway <mark.pittaway@gmail.com>
2023-01-31 19:53:32 +1100

    UI: CompanyAdmin state

---
+++ assets/company-admin/actions.js
@@ -0,0 +1,32 @@
+import {gettext, notify, errorHandler} from 'utils';
+import server from 'server';
+import {initViewData as initUserViewData, setError, fetchUsers} from 'users/actions';
+
+export const INIT_VIEW_DATA = 'INIT_VIEW_DATA';
+export function initViewData(data) {
+    return (dispatch) => {
+        dispatch({type: INIT_VIEW_DATA, data: data});
+        dispatch(initUserViewData(data));
+    };
+}
+
+export const SET_SECTION = 'SET_SECTION';
+export function setSection(id) {
+    return {type: SET_SECTION, id: id};
+}
+
+export const SET_PRODUCT_FILTER = 'SET_PRODUCT_FILTER';
+export function setProductFilter(id) {
+    return function(dispatch) {
+        dispatch({type: SET_PRODUCT_FILTER, id: id});
+        dispatch(fetchUsers());
+    };
+}
+
+export function sendProductSeatRequest(data) {
+    return function (dispatch) {
+        return server.post('/company_admin/send_product_seat_request', data)
+            .then(() => notify.success(gettext('Product Seat request sent')))
+            .catch((error) => errorHandler(error, dispatch, setError));
+    }
+}
---
+++ assets/company-admin/reducers.js
@@ -0,0 +1,60 @@
+import {INIT_VIEW_DATA, SET_PRODUCT_FILTER, SET_SECTION} from './actions';
+
+import {RENDER_MODAL, CLOSE_MODAL, MODAL_FORM_VALID, MODAL_FORM_INVALID} from 'actions';
+import {modalReducer} from 'reducers';
+import {searchReducer} from 'search/reducers';
+import userReducer from 'users/reducers';
+
+const initialState = {
+    user: null,
+    query: null,
+    users: [],
+    usersById: {},
+    activeUserId: null,
+    isLoading: false,
+    totalUsers: null,
+    activeQuery: null,
+    companies: [],
+    company: null,
+    sort: null,
+    sortDirection: 1,
+    modal: modalReducer(),
+    search: searchReducer(),
+
+    userToEdit: null,
+    errors: null,
+
+    sectionId: 'my_company',
+    products: [],
+    productId: null,
+}
+
+export function companyAdminReducer(state = initialState, action) {
+    switch (action.type) {
+        case INIT_VIEW_DATA:
+            return {
+                ...userReducer(state, action),
+                products: action.data.products,
+                company: action.data.companyId,
+            };
+        case SET_PRODUCT_FILTER:
+            return {
+                ...state,
+                productId: action.id,
+            };
+        case SET_SECTION:
+            return {
+                ...state,
+                sectionId: action.id,
+            };
+
+        case RENDER_MODAL:
+        case CLOSE_MODAL:
+        case MODAL_FORM_VALID:
+        case MODAL_FORM_INVALID:
+            return {...state, modal: modalReducer(state.modal, action)};
+
+        default:
+            return userReducer(state, action);
+    }
+}
