97581700bc18f2b17a3ff030c005b1ec5e443912
Mark Pittaway <mark.pittaway@gmail.com>
2023-03-14 19:19:51 +1100

    [SDESK-6847] fix: Update moment libraries to fix daylight saving changes

--- webpack.config.js
+++ webpack.config.js
@@ -78,6 +78,9 @@ const config = {
             path.resolve(__dirname, 'assets'),
             'node_modules',
         ],
+        alias: {
+            'moment-timezone': 'moment-timezone/builds/moment-timezone-with-data-10-year-range',
+        },
     },
     plugins: [
         new ManifestPlugin(),
