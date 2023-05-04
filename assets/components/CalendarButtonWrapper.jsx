a3126052750e8a029b56ac828002741ab0574079
devketanpro <73937490+devketanpro@users.noreply.github.com>
2023-05-04 13:42:22 +0530

    Fix : Agenda section shows blank if user language is French [CPNHUB-231] (#356)
    * Fix : Agenda section shows blank if user language is French [CPNHUB-231]
    
    * update prop name
    
    * use custum input for format datetime
    
    * change default config

--- assets/components/CalendarButtonWrapper.jsx
+++ assets/components/CalendarButtonWrapper.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import moment from 'moment';
+import {AGENDA_DATE_PICKER_FORMAT_SHORT} from '../utils';
 
 class CalendarButtonWrapper extends React.Component {
     render() {
@@ -8,7 +10,7 @@ class CalendarButtonWrapper extends React.Component {
             <button className={
                 classNames('btn btn-outline-primary btn-sm me-3 align-items-center px-2 btn-with-icon', {'active': this.props.active})}
             onClick={this.props.onClick}>
-                {this.props.value}
+                {moment(this.props.value).format(AGENDA_DATE_PICKER_FORMAT_SHORT)}
                 <i className={classNames('icon-small--arrow-down ms-1', {'icon--white': this.props.active})}></i>
             </button>
         );
