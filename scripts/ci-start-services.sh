bd7842a6b765e384e7d149b7a828e00f6deb35ca
Mark Pittaway <mark.pittaway@gmail.com>
2023-05-08 19:42:20 +1000

    fix: Change to e2e folder before starting http server

--- scripts/ci-start-services.sh
+++ scripts/ci-start-services.sh
@@ -3,10 +3,10 @@
 docker-compose -f docker-compose-services.yml up -d
 while ! curl -sfo /dev/null 'http://localhost:9200/'; do echo -n '.' && sleep .5; done
 
+cd e2e
 npm run start-client-server &
-
 sleep 2
 
-cd e2e/server
+cd server
 honcho start &
-cd ../
+cd ../../
