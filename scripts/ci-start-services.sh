a22b5f6752c6e30f2f5188e87516de48713c246e
Mark Pittaway <mark.pittaway@gmail.com>
2023-05-08 19:32:19 +1000

    fix(ci-start-services): Remove unused env var checks

--- scripts/ci-start-services.sh
+++ scripts/ci-start-services.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ "$RUN_SERVICES" == "true" ]; then
-    docker-compose -f docker-compose-services.yml up -d
-    while ! curl -sfo /dev/null 'http://localhost:9200/'; do echo -n '.' && sleep .5; done
-fi
+docker-compose -f docker-compose-services.yml up -d
+while ! curl -sfo /dev/null 'http://localhost:9200/'; do echo -n '.' && sleep .5; done
 
-if [ "$E2E" == "true" ]; then
-    cd e2e/server
-    honcho start &
-    cd ../../
-fi
+npm run start-client-server &
+
+sleep 2
+
+cd e2e/server
+honcho start &
+cd ../
