72c1ca1a7159d691284e95cf38dd6c92e0dd4ffb
devketanpro <devketanpro11@gmail.com>
2023-05-24 12:23:03 +0530

    refactored code

--- assets/company-admin/components/CompanyDetailsProductRow.jsx
+++ assets/company-admin/components/CompanyDetailsProductRow.jsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
-import {gettext} from 'utils';
-import {getConfig} from 'utils';
-
+import {gettext, getConfig} from 'utils';
 export function CompanyDetailsProductRow({seat, onNameClicked, showSeatRequestModal}) {
     return (
         <tr>
