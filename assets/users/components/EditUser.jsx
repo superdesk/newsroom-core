369b577cf51b447bb6cd8cf6ab41a081772d859c
devketanpro <devketanpro11@gmail.com>
2023-05-30 15:21:29 +0530

    make the following strings from the company management view available in Transifex [CPCN-164]

--- assets/users/components/EditUser.jsx
+++ assets/users/components/EditUser.jsx
@@ -198,7 +198,7 @@ function EditUserComponent({
                                         <div className="form-group">
                                             <CheckboxInput
                                                 name={`sections.${section._id}`}
-                                                label={section.name}
+                                                label={gettext(section.name)}
                                                 value={get(user, `sections.${section._id}`) === true}
                                                 onChange={onChange}
                                             />
@@ -219,7 +219,7 @@ function EditUserComponent({
                                 )).map((section) => (
                                     <React.Fragment key={section._id}>
                                         <div className="list-item__preview-subheading">
-                                            {section.name}
+                                            {gettext(section.name)}
                                         </div>
                                         {products.filter(
                                             (product) => product.product_type === section._id &&
