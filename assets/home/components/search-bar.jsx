cd1ee88add91ac91b00dfec8523c19adee4dfe9e
FritzSF <vladimir.stefanovic@sourcefabric.org>
2023-05-31 13:44:23 +0200

    lint fix

--- assets/home/components/search-bar.jsx
+++ assets/home/components/search-bar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {gettext} from 'utils';
+import classNames from 'classnames';
 
 export class SearchBar extends React.PureComponent {
     constructor(props) {
@@ -31,12 +32,12 @@ export class SearchBar extends React.PureComponent {
                                         className="search__button-clear"
                                         title={gettext('Clear search')}>
                                         <svg fill="none" height="18" viewBox="0 0 18 18" width="18" xmlns="http://www.w3.org/2000/svg">
-                                            <path clip-rule="evenodd" d="m9 18c4.9706 0 9-4.0294 9-9 0-4.97056-4.0294-9-9-9-4.97056 0-9 4.02944-9 9 0 4.9706 4.02944 9 9 9zm4.9884-12.58679-3.571 3.57514 3.5826 3.58675-1.4126 1.4143-3.58252-3.5868-3.59233 3.5965-1.41255-1.4142 3.59234-3.59655-3.54174-3.54592 1.41254-1.41422 3.54174 3.54593 3.57092-3.57515z" fill="var(--color-text)" fill-rule="evenodd" opacity="1"/>
+                                            <path clipRule="evenodd" d="m9 18c4.9706 0 9-4.0294 9-9 0-4.97056-4.0294-9-9-9-4.97056 0-9 4.02944-9 9 0 4.9706 4.02944 9 9 9zm4.9884-12.58679-3.571 3.57514 3.5826 3.58675-1.4126 1.4143-3.58252-3.5868-3.59233 3.5965-1.41255-1.4142 3.59234-3.59655-3.54174-3.54592 1.41254-1.41422 3.54174 3.54593 3.57092-3.57515z" fill="var(--color-text)" fillRule="evenodd" opacity="1"/>
                                         </svg>
                                     </button>
                                 )}
                                 <button className="search__button-submit" type="submit" title={gettext('Search')}>
-                                    <i class="icon--search"></i>
+                                    <i className="icon--search"></i>
                                 </button>
                             </div>
                         </form>
