dc18c2734ac4e9bbd649652c480647f55c68aa81
devketanpro <devketanpro11@gmail.com>
2023-06-01 10:42:51 +0530

    update functionality in the Ui for highlighted text

--- assets/ui/components/ArticleHeadline.jsx
+++ assets/ui/components/ArticleHeadline.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 export default function ArticleHeadline({item}) {
     return (
-        item.headline && <h2 className='wire-column__preview__headline'>{item.headline}</h2> || null
+        item.es_highlight && item.es_highlight.headline ? <div
+            dangerouslySetInnerHTML={({__html: item.es_highlight.headline[0]})}
+        /> : item.headline && <h2 className='wire-column__preview__headline'>{item.headline}</h2> || null
     );
 }
 
