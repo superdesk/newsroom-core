6fdbec4f14aabf98b0b5da3cd08c85e46ba2c0dd
devketanpro <devketanpro11@gmail.com>
2023-06-01 19:59:15 +0530

    addressed comment

--- assets/ui/components/ArticleHeadline.jsx
+++ assets/ui/components/ArticleHeadline.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function ArticleHeadline({item}) {
-    return (
-        <h2 className='wire-column__preview__headline'>
+    return item.headline ? (
+        <h2 className="wire-column__preview__headline">
             {item.es_highlight && item.es_highlight.headline ? (
                 <span dangerouslySetInnerHTML={{__html: item.es_highlight.headline[0]}} />
-            ) : item.headline ? (
-                item.headline
-            ) : null}
+            ) : item.headline}
         </h2>
-    );
+    ) : null;
 }
 
 ArticleHeadline.propTypes = {
--- assets/ui/components/ArticleSlugline.jsx
+++ assets/ui/components/ArticleSlugline.jsx
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types';
 import {getSlugline} from 'utils';
 
 export default function ArticleSlugline({item}) {
-    const slugline =   item.es_highlight && item.es_highlight.slugline ? <div
-        dangerouslySetInnerHTML={({__html: item.es_highlight.slugline[0]})}
-    /> : getSlugline(item, true);
-
-    return <span className="wire-column__preview__slug">{slugline}</span>;
+    const slugline = getSlugline(item, true);
+  
+    return slugline ? (
+        <div className="wire-column__preview__slugline">
+            {item.es_highlight && item.es_highlight.slugline ? (
+                <span dangerouslySetInnerHTML={{__html: item.es_highlight.slugline[0]}} />
+            ) : slugline}
+        </div>
+    ) : null;
 }
 
 ArticleSlugline.propTypes = {
