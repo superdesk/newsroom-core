ee601046fd53e088aa1b6396cf66e9da5b923bfe
devketanpro <73937490+devketanpro@users.noreply.github.com>
2023-06-08 18:39:24 +0530

    update fields setting to show full body text in highlight [CPCN-17] (#395)
    * update fields setting to show full body text in highlight
    
    * configure highlighting fields based on selected fields in advanced search
    
    * reformat code
    
    * addressed comment

--- assets/wire/components/WireListItem.jsx
+++ assets/wire/components/WireListItem.jsx
@@ -265,9 +265,7 @@ class WireListItem extends React.Component {
 
                         {isExtended && (
                             <div className="wire-articles__item__text">
-                                {item.es_highlight ? <div
-                                    dangerouslySetInnerHTML={({__html:item.es_highlight.body_html && item.es_highlight.body_html[0]})}
-                                /> : <p>{shortText(item, 40, listConfig)}</p>}
+                                {<p>{shortText(item, 40, listConfig)}</p>}
                             </div>
                         )}
 
