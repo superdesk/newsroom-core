7fc27bc4bef3761b7373f7aa5fef515efcbb2e18
devketanpro <devketanpro11@gmail.com>
2023-06-16 15:06:51 +0530

    Fix : More News button should always appear for users of type 'Administrator' [NHUB-307]

--- newsroom/wire/views.py
+++ newsroom/wire/views.py
@@ -16,7 +16,7 @@ from superdesk import get_resource_service
 from superdesk.default_settings import strtobool
 
 from newsroom.navigations.navigations import get_navigations_by_company
-from newsroom.products.products import get_products_by_company
+from newsroom.products.products import get_products_by_company, get_product_by_id
 from newsroom.wire import blueprint
 from newsroom.wire.utils import update_action_list
 from newsroom.auth import get_company, get_user, get_user_id
@@ -130,6 +130,15 @@ def delete_dashboard_caches():
         app.cache.delete(f"{HOME_ITEMS_CACHE_KEY}{company['_id']}")
 
 
+def get_products_by_card(cards, user):
+    if user.get("user_type") == "administrator" and not user.get("company"):
+        products = []
+        for card in cards:
+            products += get_product_by_id(card.get("config", {}).get("product", ""))
+        return products
+    return []
+
+
 def get_home_data():
     user = get_user()
     company = get_company(user)
@@ -139,7 +148,7 @@ def get_home_data():
 
     return {
         "cards": cards,
-        "products": get_products_by_company(company) if company else [],
+        "products": get_products_by_company(company) if company else get_products_by_card(cards, user),
         "user": str(user["_id"]) if user else None,
         "userProducts": user.get("products") if user else [],
         "userType": user.get("user_type"),
