c72bf10dd934c3abce80450da089d4bc2ef0d4e8
devketanpro <devketanpro11@gmail.com>
2023-08-08 18:32:34 +0530

    fix lint

--- assets/agenda/utils.ts
+++ assets/agenda/utils.ts
@@ -579,7 +579,7 @@ export function getHighlightedName(item: any) {
  * @return {String}
  */
 export function getDescription(item: any, plan: any) {
-    return plan ? plan.description_text : (item ? item.definition_short : "")
+    return plan ? plan.description_text : (item ? item.definition_short : '');
 }
 
 /**
