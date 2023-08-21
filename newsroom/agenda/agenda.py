79a08c2f0cf50dd2f090d4f33e4201c8b918afd4
devketanpro <devketanpro11@gmail.com>
2023-08-21 09:54:58 +0530

    Filtering by STT's coverage statuses (instead of current coverage statuses) [STTNHUB-203]

--- newsroom/agenda/agenda.py
+++ newsroom/agenda/agenda.py
@@ -552,17 +552,40 @@ def _filter_terms(filters, item_type):
                     nested_query(
                         path="coverages",
                         query={
-                            "bool": {"filter": [{"terms": {"coverages.coverage_status": ["coverage not decided yet"]}}]}
+                            "bool": {
+                                "filter": [
+                                    {
+                                        "terms": {
+                                            "coverages.coverage_status": [
+                                                "coverage not decided yet",
+                                                "coverage upon request",
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
                         },
                         name="coverage_status",
                     )
                 )
             elif val == ["not planned"]:
-                must_term_filters.append(
+                must_not_term_filters.append(
                     nested_query(
                         path="coverages",
                         query={
-                            "bool": {"filter": [{"terms": {"coverages.coverage_status": ["coverage not intended"]}}]}
+                            "bool": {
+                                "filter": [
+                                    {
+                                        "terms": {
+                                            "coverages.coverage_status": [
+                                                "coverage intended",
+                                                "coverage not decided yet",
+                                                "coverage upon request",
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
                         },
                         name="coverage_status",
                     )
