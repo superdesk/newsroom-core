e71c06dd029931575deab5f399588ac1279dafd2
Mark Pittaway <mark.pittaway@gmail.com>
2023-08-29 15:37:57 +1000

    api: SAML changes

--- newsroom/auth/utils.py
+++ newsroom/auth/utils.py
@@ -10,8 +10,14 @@ from flask_babel import _
 from newsroom.user_roles import UserRole
 from superdesk.utc import utcnow
 from newsroom.auth import get_user, get_company
-from newsroom.types import UserData
-from newsroom.utils import get_random_string, is_valid_user
+from newsroom.types import User, UserData, Company, AuthProvider, AuthProviderType
+from newsroom.utils import (
+    get_random_string,
+    is_valid_user,
+    is_company_enabled,
+    is_company_expired,
+    is_account_enabled,
+)
 from newsroom.email import (
     send_validate_account_email,
     send_reset_password_email,
@@ -30,20 +36,40 @@ def sign_user_by_email(
     redirect_on_error: str = "auth.login",
     create_missing: bool = False,
     userdata: Optional[UserData] = None,
+    validate_login_attempt: bool = False,
 ) -> werkzeug.Response:
     users = superdesk.get_resource_service("users")
-    user: UserData = users.find_one(req=None, email=email)
+    user: User = users.find_one(req=None, email=email)
 
     if user is None and create_missing and userdata is not None:
         user = userdata.copy()
         user["is_enabled"] = True
         users.create([user])
 
-    if user is None:
-        flask.flash(_("User not found"), "danger")
+    def redirect_with_error(error_str):
+        flask.session.pop("_flashes", None)
+        flask.flash(error_str, "danger")
         return flask.redirect(flask.url_for(redirect_on_error, user_error=1))
 
+    if user is None:
+        return redirect_with_error(_("User not found"))
+
     assert "_id" in user
+
+    if validate_login_attempt:
+        company = get_company(user)
+        auth_provider = get_company_auth_provider(company)
+
+        if company is None:
+            return redirect_with_error(_("No Company assigned"))
+        elif not is_company_enabled(user, company):
+            return redirect_with_error(_("Company is disabled"))
+        elif is_company_expired(user, company):
+            return redirect_with_error(_("Company has expired"))
+        elif not is_account_enabled(user):
+            return redirect_with_error(_("Account is disabled"))
+        elif auth_provider["auth_type"] != AuthProviderType.SAML.value:
+            return redirect_with_error(_("Invalid login type, SAML not enabled for your user"))
 
     users.system_update(
         user["_id"],
@@ -59,7 +85,7 @@ def sign_user_by_email(
     return flask.redirect(flask.url_for(redirect_on_success))
 
 
-def start_user_session(user: UserData, permanent=False):
+def start_user_session(user: User, permanent=False):
     flask.session["user"] = str(user["_id"])  # str to avoid serialization issues
     flask.session["name"] = "{} {}".format(user.get("first_name"), user.get("last_name"))
     flask.session["user_type"] = user["user_type"]
@@ -168,3 +194,10 @@ def user_can_manage_company(company_id) -> bool:
         if user:
             return str(user.get("company")) == str(company_id) and company_id
     return False
+
+
+def get_company_auth_provider(company: Optional[Company] = None) -> AuthProvider:
+    providers: Dict[str, AuthProvider] = {provider["_id"]: provider for provider in app.config["AUTH_PROVIDERS"]}
+
+    provider_id = (company or {}).get("auth_provider") or "newshub"
+    return providers.get(provider_id) or providers["newshub"]
