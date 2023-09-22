a3d68c5af58174fda6e4c7e6c2946510466c1f82
devketanpro <devketanpro11@gmail.com>
2023-09-22 09:49:30 +0530

    handle json decoding

--- newsroom/users/views.py
+++ newsroom/users/views.py
@@ -80,27 +80,26 @@ def user_profile():
 def search():
     lookup = None
     sort = None
-    try:
-        if flask.request.args.get("q"):
-            regex = re.compile(".*{}.*".format(flask.request.args.get("q")), re.IGNORECASE)
-            lookup = {"$or": [{"first_name": regex}, {"last_name": regex}]}
+    if flask.request.args.get("q"):
+        regex = re.compile(".*{}.*".format(flask.request.args.get("q")), re.IGNORECASE)
+        lookup = {"$or": [{"first_name": regex}, {"last_name": regex}]}
 
-        if flask.request.args.get("ids"):
-            lookup = {"_id": {"$in": (flask.request.args.get("ids") or "").split(",")}}
+    if flask.request.args.get("ids"):
+        lookup = {"_id": {"$in": (flask.request.args.get("ids") or "").split(",")}}
 
-        if flask.request.args.get("sort"):
-            sort = flask.request.args.get("sort")
+    if flask.request.args.get("sort"):
+        sort = flask.request.args.get("sort")
 
-        where_param = flask.request.args.get("where")
-        if where_param:
+    where_param = flask.request.args.get("where")
+    if where_param:
+        try:
             where = json.loads(where_param)
             if where.get("company"):
                 lookup = {"company": where["company"]}
             if where.get("products._id"):
                 lookup = {"products._id": where["products._id"]}
-    except json.JSONDecodeError as e:
-        return jsonify({"error": "Invalid JSON in parameters: {}".format(str(e))}), 400
-
+        except json.JSONDecodeError as e:
+            return jsonify({"error": "Invalid 'where' parameter. JSON decoding failed: {}".format(str(e))}), 400
     if is_current_user_company_admin():
         # Make sure this request only searches for the current users company
         company = get_company()
