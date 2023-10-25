cdcc712ca179a2e3928bbdfb0168ea1b4336a553
thecalcc <me@kmarkov.io>
2023-10-25 10:49:43 +0300

    Changes after review v1

--- assets/wire/components/filters/FilterGroup.tsx
+++ assets/wire/components/filters/FilterGroup.tsx
@@ -5,7 +5,8 @@ import {Skeleton} from 'primereact/skeleton';
 
 import NavGroup from './NavGroup';
 import FilterItem from './FilterItem';
-import {WithPagination} from 'search/components/WithPagination';
+import {WithPagination} from 'components/pagination/WithPagination';
+import {searchIcon} from 'search/components/search-icon';
 
 const LIMIT = 5;
 type IBucket = {key: string, doc_count: string};
@@ -88,16 +89,13 @@ export default function FilterGroup({group, activeFilter, aggregations, toggleFi
                         }}
                         value={searchTerm}
                         type="text"
-                        name="q"
                         className="search__input form-control"
                         placeholder={gettext('Search Filters')}
                         aria-label={gettext('Search Filters')}
                     />
                     <div className="search__form-buttons">
                         <button className="search__button-clear" aria-label={gettext('Clear search')} type="button">
-                            <svg fill="none" height="18" viewBox="0 0 18 18" width="18" xmlns="http://www.w3.org/2000/svg">
-                                <path clipRule="evenodd" d="m9 18c4.9706 0 9-4.0294 9-9 0-4.97056-4.0294-9-9-9-4.97056 0-9 4.02944-9 9 0 4.9706 4.02944 9 9 9zm4.9884-12.58679-3.571 3.57514 3.5826 3.58675-1.4126 1.4143-3.58252-3.5868-3.59233 3.5965-1.41255-1.4142 3.59234-3.59655-3.54174-3.54592 1.41254-1.41422 3.54174 3.54593 3.57092-3.57515z" fill="var(--color-text)" fillRule="evenodd" opacity="1"></path>
-                            </svg>
+                            {searchIcon}
                         </button>
                     </div>
                 </div>
