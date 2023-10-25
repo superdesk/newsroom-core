1edfacca78c57e800bccb3099e6218aa7478f209
Konstantin Markov <maga.ot.oz@protonmail.com>
2023-10-25 13:01:43 +0300

    Filter search & pagination (#628)

--- assets/components/PersonalizeHomeModal.tsx
+++ assets/components/PersonalizeHomeModal.tsx
@@ -14,6 +14,7 @@ import {getCurrentUser} from 'company-admin/selectors';
 import {IAgendaState} from 'agenda/reducers';
 import {ITopic} from 'interfaces/topic';
 import {IPersonalizedDashboardsWithData} from 'home/reducers';
+import {searchIcon} from 'search/components/search-icon';
 
 interface IMapStateProps {
     topics: Array<ITopic>;
@@ -302,9 +303,7 @@ class PersonalizeHomeModal extends React.Component<IProps, IState> {
                                     />
                                     <div className="search__form-buttons">
                                         <button className="search__button-clear" aria-label={gettext('Clear search')} type="reset">
-                                            <svg fill="none" height="18" viewBox="0 0 18 18" width="18" xmlns="http://www.w3.org/2000/svg">
-                                                <path clipRule="evenodd" d="m9 18c4.9706 0 9-4.0294 9-9 0-4.97056-4.0294-9-9-9-4.97056 0-9 4.02944-9 9 0 4.9706 4.02944 9 9 9zm4.9884-12.58679-3.571 3.57514 3.5826 3.58675-1.4126 1.4143-3.58252-3.5868-3.59233 3.5965-1.41255-1.4142 3.59234-3.59655-3.54174-3.54592 1.41254-1.41422 3.54174 3.54593 3.57092-3.57515z" fill="var(--color-text)" fillRule="evenodd" opacity="1"></path>
-                                            </svg>
+                                            {searchIcon}
                                         </button>
                                     </div>
                                 </form>
