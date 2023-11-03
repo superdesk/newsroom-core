5163435ef17e1b374d417ee3b96d7404b1bee1dc
Petr Jašek <jasekpetr@gmail.com>
2023-11-03 11:31:11 +0100

    make section required in products reference (#654)
    * make section required in products reference
    
    CPCN-444

--- newsroom/users/users.py
+++ newsroom/users/users.py
@@ -8,6 +8,7 @@ from typing import Dict, Optional, List
 from flask import current_app as app, session, abort, request
 from flask_babel import gettext
 from werkzeug.exceptions import BadRequest
+from newsroom.products.types import PRODUCT_TYPES
 
 from newsroom.types import Company, ProductRef, User
 from newsroom.auth import get_user_id, get_user, get_company_from_user, SessionAuth
@@ -104,8 +105,8 @@ class UsersResource(newsroom.Resource):
             "schema": {
                 "type": "dict",
                 "schema": {
-                    "_id": newsroom.Resource.rel("products"),
-                    "section": {"type": "string", "default": "wire"},
+                    "_id": newsroom.Resource.rel("products", required=True),
+                    "section": {"type": "string", "required": True, "allowed": PRODUCT_TYPES},
                 },
             },
         },
