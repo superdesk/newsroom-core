cc402e63cced830af0a8344d7366f052d40280f5
devketanpro <devketanpro11@gmail.com>
2023-12-20 13:38:32 +0530

    minor change

--- newsroom/topics/topics.py
+++ newsroom/topics/topics.py
@@ -86,7 +86,7 @@ class TopicsService(newsroom.Service):
             subscriber = next(
                 (
                     subscriber
-                    for subscriber in updates.get("subscribers", [])
+                    for subscriber in updates.get("subscribers", original.get("subscribers", []))
                     if subscriber["user_id"] == original["user"]
                 ),
                 None,
