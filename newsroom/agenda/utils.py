0e439e3673fbf0ce531201fddb07630674478abf
devketanpro <devketanpro11@gmail.com>
2024-01-10 16:07:44 +0530

    use gettext for translation

--- newsroom/agenda/utils.py
+++ newsroom/agenda/utils.py
@@ -167,11 +167,11 @@ def get_planning_coverages(item, plan_id):
 def get_coverage_status(coverage: Dict[str, Any]) -> str:
     coverage_status = coverage.get("coverage_status")
     if coverage_status == "coverage intended":
-        return "Planned"
+        return gettext("Planned")
     elif coverage_status in ("coverage not decided yet", "coverage upon request"):
-        return "Not decided"
+        return gettext("Not decided")
     elif coverage_status == "coverage not intended":
-        return "Cancelled"
+        return gettext("Cancelled")
     else:
         return ""
 
