a71bebea37bfce45718105f5c7efe59cfaf42527
Petr Jašek <jasekpetr@gmail.com>
2024-02-13 10:44:24 +0100

    always show same date on event (#761)
    it was different based on coverages but then it wouldn't
    be consistent with preview etc.
    
    CPCN-645

--- assets/utils.tsx
+++ assets/utils.tsx
@@ -157,7 +157,7 @@ export function render(store: any, App: any, element?: any, props?: any) {
  * @param {Object} params
  * @return {String}
  */
-export function gettext(text: string, params?: {[key: string]: any}): string {
+export function gettext(text: string, params?: {[key: string]: string | number}): string {
     let translated = get(window.translations, text, text);
 
     if (params) {
