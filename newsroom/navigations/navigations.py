e37eaf6c63bd85c3d25d30805a3f9a730f8a6b51
Ketan <73937490+devketanpro@users.noreply.github.com>
2024-02-15 18:05:30 +0530

    FIX: show all global topics to user_type Adminstrator [CPCN-617] (#795)
    show all global topics to user_type Adminstrator

--- newsroom/navigations/navigations.py
+++ newsroom/navigations/navigations.py
@@ -2,6 +2,7 @@ import newsroom
 from newsroom.products.products import get_products_by_company, get_products_by_user
 import superdesk
 
+from newsroom.utils import is_admin
 from newsroom.types import Company, UserData
 
 
@@ -71,6 +72,10 @@ def get_navigations_by_user(user: UserData, product_type="wire", events_only=Fal
     Returns list of navigations for given user id
     Navigations will contain the list of product ids
     """
+
+    if is_admin(user):
+        return list(superdesk.get_resource_service("navigations").get(req=None, lookup={}))
+
     products = get_products_by_user(user, product_type, None)
 
     # Get the navigation ids used across products
