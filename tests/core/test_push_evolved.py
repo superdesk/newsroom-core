f36a8f5d3a9cf02b9f712abce76d29fc115f0867
Petr Jašek <jasekpetr@gmail.com>
2024-03-06 13:10:52 +0100

    fix typo in index field name (#824)
    * fix typo in index field name
    
    CPCN-599
    
    * make it configurable

--- tests/core/test_push_evolved.py
+++ tests/core/test_push_evolved.py
@@ -1,4 +1,5 @@
 from flask import json
+from unittest.mock import patch
 
 
 def ids(items):
@@ -17,42 +18,43 @@ def test_evolved_from_order(client, app):
         assert 200 == resp.status_code
         return resp.json["_items"]
 
-    push_item(
-        {
-            "type": "text",
-            "guid": "item3",
-            "evolvedfrom": "item2",
-        }
-    )
+    with patch.dict(app.config, {"PUSH_FIX_UPDATES": True}):
+        push_item(
+            {
+                "type": "text",
+                "guid": "item3",
+                "evolvedfrom": "item2",
+            }
+        )
 
-    push_item(
-        {
-            "type": "text",
-            "guid": "item5",
-            "evolvedfrom": "item4",
-        }
-    )
+        push_item(
+            {
+                "type": "text",
+                "guid": "item5",
+                "evolvedfrom": "item4",
+            }
+        )
 
-    items = search()
-    assert 2 == len(items)
-    assert "item3" in ids(items)
-    assert "item5" in ids(items)
+        items = search()
+        assert 2 == len(items)
+        assert "item3" in ids(items)
+        assert "item5" in ids(items)
 
-    push_item({"type": "text", "guid": "item2", "evolvedfrom": "item1"})
-    items = search()
-    assert 2 == len(items)
-    assert "item3" in ids(items)
-    assert "item5" in ids(items)
+        push_item({"type": "text", "guid": "item2", "evolvedfrom": "item1"})
+        items = search()
+        assert 2 == len(items)
+        assert "item3" in ids(items)
+        assert "item5" in ids(items)
 
-    push_item({"type": "text", "guid": "item1"})
-    items = search()
-    assert 2 == len(items)
-    assert "item3" in ids(items)
-    assert "item5" in ids(items)
+        push_item({"type": "text", "guid": "item1"})
+        items = search()
+        assert 2 == len(items)
+        assert "item3" in ids(items)
+        assert "item5" in ids(items)
 
-    push_item({"type": "text", "guid": "item4", "evolvedfrom": "item3"})
-    items = search()
-    assert 1 == len(items)
-    assert "item5" in ids(items)
-    assert ["item1", "item2", "item3", "item4"] == items[0]["ancestors"]
-    assert "item1" == items[0]["original_id"]
+        push_item({"type": "text", "guid": "item4", "evolvedfrom": "item3"})
+        items = search()
+        assert 1 == len(items)
+        assert "item5" in ids(items)
+        assert ["item1", "item2", "item3", "item4"] == items[0]["ancestors"]
+        assert "item1" == items[0]["original_id"]
