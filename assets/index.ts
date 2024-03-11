6de580c108a71e9084bc70671f9f8415d2213b48
dzonidoo <nikola71092@gmail.com>
2024-03-11 12:52:35 +0100

    changes after review

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,29 @@
-import {isTouchDevice} from 'utils';
+import {isTouchDevice, gettext} from 'utils';
 import 'primereact/resources/primereact.min.css';
 
 if (isTouchDevice()) {
     document.documentElement.classList.add('no-touch');
 }
+
+let element = document.getElementById('nav');
+let pinButton = document.getElementById('pin-btn');
+
+if (pinButton != null) {
+    pinButton.onclick = handleNavExpanded;
+}
+
+if (sessionStorage.getItem('navigation-pinned')) {
+    handleNavExpanded();
+}
+
+function handleNavExpanded() {
+    if (element?.classList.contains("nav-block--pinned")) {
+        sessionStorage.removeItem('navigation-pinned');
+        element.classList.remove("nav-block--pinned");
+        pinButton?.setAttribute('title', gettext('Expand'));
+    } else {
+        sessionStorage.setItem('navigation-pinned', 'true');
+        element?.classList.add("nav-block--pinned");
+        pinButton?.setAttribute('title', gettext('Collapse'));
+    }
+};
