d6ed7ae5daf7665243865a3e83fd2f3353f49f5b
dzonidoo <nikola71092@gmail.com>
2024-03-13 16:41:11 +0100

    chages after review

--- assets/index.ts
+++ assets/index.ts
@@ -9,21 +9,27 @@ const element = document.getElementById('nav');
 const pinButton = document.getElementById('pin-btn');
 
 if (pinButton != null) {
-    pinButton.onclick = handleNavExpanded;
+    pinButton.addEventListener('click', () => {
+        handleNavToggle();
+    });
 }
 
 if (sessionStorage.getItem('navigation-pinned')) {
-    handleNavExpanded();
+    handleNavToggle();
 }
 
-function handleNavExpanded() {
-    if (element?.classList.contains('nav-block--pinned')) {
+function handleNavToggle() {
+    if (element == null || pinButton == null) {
+        return null;
+    }
+
+    if (element.classList.contains('nav-block--pinned')) {
         sessionStorage.removeItem('navigation-pinned');
         element.classList.remove('nav-block--pinned');
-        pinButton?.setAttribute('title', gettext('Expand'));
+        pinButton.setAttribute('title', gettext('Expand'));
     } else {
         sessionStorage.setItem('navigation-pinned', 'true');
-        element?.classList.add('nav-block--pinned');
-        pinButton?.setAttribute('title', gettext('Collapse'));
+        element.classList.add('nav-block--pinned');
+        pinButton.setAttribute('title', gettext('Collapse'));
     }
 }
