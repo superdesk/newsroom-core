6845cca15c851413542a051815fd89faff47b77a
Helmy Giacoman <helmygb@gmail.com>
2024-05-28 11:43:44 +0200

    [CPCN-177] - Add `Source` column to Content Activity report (#904)
    * Minor code clean up
    
    CPCN-177
    
    * Add `Source` column to Content Activity report
    
    This adds the column `source` to the content activity report both in the frontend UI and in the CSV export.
    
    CPCN-177
    
    * Fix format with black
    
    CPCN-177
    
    * Implement typings for props and remove `_.get` usage
    
    CPCN-177
    
    * Extract report filter into separate component
    
    The intention is to simplify and to improve the `ContentActivity.tsx` component from using UNSAFE cycle methods.
    
    CPCN-177
    
    * Remove unnecessary state of `ContentActivity.tsx`
    
    CPCN-177
    
    * Small UI/UX improvements
    
    CPCN-177

---
+++ assets/company-reports/components/ContentActivityFilters.tsx
@@ -0,0 +1,137 @@
+import React, {useEffect, useMemo} from 'react';
+import {Dispatch} from 'redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {gettext} from 'utils';
+
+import MultiSelectDropdown from 'components/MultiSelectDropdown';
+import {REPORTS as REPORTS_API_ENDPOINTS} from 'company-reports/actions';
+import {toggleFilter, fetchAggregations} from '../actions';
+import CalendarButton from 'components/CalendarButton';
+import moment from 'moment';
+
+export const ContentActivityFilters = () => {
+    const dispatch = useDispatch<Dispatch<any>>();
+    const {
+        sections,
+        reportParams,
+        apiEnabled,
+        companies,
+        reportAggregations
+    } = useSelector((state: any) => state);
+
+    const internalToggleFilter = (filterName: string, value: any) => {
+        dispatch(toggleFilter(filterName, value));
+    };
+
+    const fetchReportAggregations = () => {
+        dispatch(fetchAggregations(REPORTS_API_ENDPOINTS['content-activity']));
+    };
+
+    const onChangeSection = (field: string, value: string) => {
+        internalToggleFilter('section', value);
+        fetchReportAggregations();
+    };
+
+    const onDateChange = (value: any) => {
+        internalToggleFilter('date_from', value);
+        fetchReportAggregations();
+    };
+
+    const genreOptions = useMemo(() => {
+        return (reportAggregations?.genres ?? [])
+            .sort()
+            .map((genre: string) => ({
+                label: genre,
+                value: genre,
+            }));
+    }, [reportAggregations?.genres]);
+
+    const companyOptions = useMemo(() => {
+        return companies
+            .filter((company: any) => reportAggregations?.companies.includes(company._id))
+            .map((company: any) => ({
+                label: company.name,
+                value: company.name,
+            }));
+    }, [companies, reportAggregations?.companies]);
+
+    const filters = [
+        {
+            field: 'section',
+            label: gettext('Section'),
+            options: sections
+                .filter((section: any) =>
+                    ['wire', 'monitoring'].includes(section.group) ||
+                    (section.group === 'api' && apiEnabled)
+                )
+                .map((section: any) => ({
+                    label: section.name,
+                    value: section.name,
+                })),
+            onChange: onChangeSection,
+            showAllButton: false,
+            multi: false,
+            default: sections[0]?.name,
+        },
+        {
+            field: 'genre',
+            label: gettext('Genres'),
+            options: genreOptions,
+            onChange: internalToggleFilter,
+            showAllButton: true,
+            multi: true,
+            default: [],
+        },
+        {
+            field: 'company',
+            label: gettext('Companies'),
+            options: companyOptions,
+            onChange: internalToggleFilter,
+            showAllButton: true,
+            multi: false,
+            default: null,
+        },
+        {
+            field: 'action',
+            label: gettext('Actions'),
+            options: [
+                {label: gettext('Download'), value: 'download'},
+                {label: gettext('Copy'), value: 'copy'},
+                {label: gettext('Share'), value: 'share'},
+                {label: gettext('Print'), value: 'print'},
+                {label: gettext('Open'), value: 'open'},
+                {label: gettext('Preview'), value: 'preview'},
+                {label: gettext('Clipboard'), value: 'clipboard'},
+                {label: gettext('API retrieval'), value: 'api'},
+            ],
+            onChange: internalToggleFilter,
+            showAllButton: true,
+            multi: true,
+            default: [],
+        }
+    ];
+
+    useEffect(fetchReportAggregations, []);
+
+    return (
+        <>
+            <div>
+                <CalendarButton
+                    key='content_activity_date'
+                    selectDate={onDateChange}
+                    activeDate={reportParams?.date_from || moment()}
+                />
+            </div>
+
+            {filters.map((filter: any) => {
+                return (
+                    <MultiSelectDropdown
+                        key={filter.field}
+                        {...filter}
+                        values={reportParams[filter.field] || filter.default}
+                    />
+                );
+            })}
+        </>
+    );
+};
