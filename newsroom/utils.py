686ee49d810aafa107c49cc1dafca1b4362513e6
devketanpro <devketanpro11@gmail.com>
2024-05-29 16:19:28 +0530

    refactore code

--- newsroom/utils.py
+++ newsroom/utils.py
@@ -18,7 +18,7 @@ from bson.errors import InvalidId
 from eve.utils import config, ParsedRequest
 from eve_elastic.elastic import parse_date, ElasticCursor
 from flask import current_app as app, json, abort, request, g, flash, session, url_for
-from flask_babel import gettext
+from flask_babel import gettext, format_date as _format_date
 
 from newsroom.types import PublicUserData, User, Company, Group, Permissions
 from newsroom.template_filters import (
@@ -201,9 +201,9 @@ def get_agenda_dates(agenda: Dict[str, Any], date_paranthesis: bool = False) -> 
 
     if all_day:
         format = get_client_format("NOTIFICATION_EMAIL_DATE_FORMAT") or "dd/MM/yyyy"
-        _start = format_datetime(start, format, rebase=False)
-        _end = format_datetime(end, format, rebase=False)
-        return "{}".format(_start) if start.date() == end.date() else "{} - {}".format(_start, _end)
+        _start = _format_date(start, format, rebase=False)
+        _end = _format_date(end, format, rebase=False)
+        return _start if start.date() == end.date() else "{} - {}".format(_start, _end)
 
     if start + timedelta(minutes=DAY_IN_MINUTES) < end:
         # Multi day event
