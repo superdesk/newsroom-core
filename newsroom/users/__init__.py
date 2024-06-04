7f98f12a5e5bdaa282896da7e64e56f2b11b211a
Petr Jašek <jasekpetr@gmail.com>
2024-06-04 16:04:25 +0200

    implement backend for pause notifications (#929)
    * implement backend for pause notifications
    
    CPCN-833 CPCN-186
    
    * add missing types

--- newsroom/users/__init__.py
+++ newsroom/users/__init__.py
@@ -4,17 +4,18 @@ from flask import Blueprint
 from flask_babel import lazy_gettext
 
 
-from .users import AuthUserService, UsersResource, UsersService, AuthUserResource
+from .users import AuthUserService, UsersResource, AuthUserResource, users_service
 
 blueprint = Blueprint("users", __name__)
 
-from . import views  # noqa
-
 
 def init_app(app):
-    superdesk.register_resource("users", UsersResource, UsersService, _app=app)
+    superdesk.register_resource("users", UsersResource, service_instance=users_service, _app=app)
     superdesk.register_resource("auth_user", AuthUserResource, AuthUserService, _app=app)
+    from . import views  # noqa
+
     app.add_template_global(views.get_view_data, "get_user_profile_data")
+
     app.settings_app(
         "users",
         lazy_gettext("User Management"),
