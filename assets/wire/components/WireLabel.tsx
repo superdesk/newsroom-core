9fdb906766120b6cd8c330a331d0c5296ac36c7b
dzonidoo <nikola71092@gmail.com>
2024-06-05 19:16:12 +0200

    chages after review

--- assets/wire/components/WireLabel.tsx
+++ assets/wire/components/WireLabel.tsx
@@ -2,28 +2,33 @@ import React from 'react';
 import classNames from  'classnames';
 import {IArticle} from 'interfaces';
 import {wireLabel} from 'agenda/utils';
+import {gettext} from 'utils';
 
 interface IProps {
     item: IArticle;
 }
 
 export default function WireLabel({item}: IProps) {
-    const classes = classNames('label label--fill label--rounded');
-
     const subjectList = item.subject || [];
     const wireItemSubjects = subjectList.filter(wireLabel);
 
     return (
         wireItemSubjects.length > 0 ? (
             <div>
-                {wireItemSubjects.map(subject => (
-                    <span
-                        className={classes + ` label-wire--${subject.code}`}
-                        key={subject.name}
-                    >
-                        {subject.name}
-                    </span>
-                ))}
+                {wireItemSubjects.map(subject => {
+                    const classes = classNames('label label--fill label--rounded', {
+                        [`label-wire--${subject.code}`]: subject.code,
+                    });
+
+                    return (
+                        <span
+                            className={classes}
+                            key={subject.code}
+                        >
+                            {gettext(subject.name)}
+                        </span>
+                    )
+                })}
             </div>
         ) : null
     );
