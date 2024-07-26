583941cccbabd18ee804463952388b79be6cc60c
Petr Jasek <jasekpetr@gmail.com>
2024-07-26 10:53:34 +0200

    add internal flag to companies
    and create public users when using sso and company is not internal
    
    STTNHUB-352

--- newsroom/companies/views.py
+++ newsroom/companies/views.py
@@ -119,7 +119,15 @@ def get_company_updates(data, original=None):
         "auth_provider": data.get("auth_provider") or original.get("auth_provider") or "newshub",
     }
 
-    for field in ["sections", "archive_access", "events_only", "restrict_coverage_info", "products", "seats"]:
+    for field in [
+        "sections",
+        "archive_access",
+        "events_only",
+        "restrict_coverage_info",
+        "products",
+        "seats",
+        "internal",
+    ]:
         if field in data:
             updates[field] = data[field]
 
