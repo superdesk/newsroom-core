fe82fd9b4960745d8e0dae49a05f373b088eb145
Helmy Giacoman <helmygb@gmail.com>
2024-07-31 15:23:17 +0200

    Simplify view and add basic tests
    SDESK-7286

--- newsroom/upload.py
+++ newsroom/upload.py
@@ -13,7 +13,7 @@ import newsroom
 from newsroom.decorator import is_valid_session, clear_session_and_redirect_to_login
 
 
-cache_for = 3600 * 24 * 7  # 7 days cache
+CACHE_MAX_AGE = 3600 * 24 * 7  # 7 days
 ASSETS_RESOURCE = "upload"
 blueprint = Blueprint(ASSETS_RESOURCE, __name__)
 
@@ -26,46 +26,52 @@ def get_file(key):
         return url_for("upload.get_upload", media_id=filename)
 
 
-@blueprint.route("/assets/<path:media_id>", methods=["GET"])
-def get_upload(media_id, filename=None):
-    # Allow access to ``/assets/<media_id>`` if PUBLIC_DASHBOARD is enabled or is a valid session
-    if not get_app_config("PUBLIC_DASHBOARD") and not is_valid_session():
-        return clear_session_and_redirect_to_login()
-
-    app = get_current_app()
-
+def get_media_file(media_id):
     try:
-        media_file = app.media.get(media_id, ASSETS_RESOURCE)
+        return get_current_app().media.get(media_id, ASSETS_RESOURCE)
     except bson.errors.InvalidId:
-        media_file = None
-    if not media_file:
-        abort(404)
+        return None
 
+
+def construct_response(media_file):
+    app = get_current_app()
     data = wrap_file(request.environ, media_file, buffer_size=1024 * 256)
+
     response = app.response_class(data, mimetype=media_file.content_type, direct_passthrough=True)
     response.content_length = media_file.length
     response.last_modified = media_file.upload_date
     response.set_etag(media_file.md5)
-    response.cache_control.max_age = cache_for
-    response.cache_control.s_max_age = cache_for
+    response.cache_control.max_age = CACHE_MAX_AGE
+    response.cache_control.s_max_age = CACHE_MAX_AGE
     response.cache_control.public = True
-
-    # Add ``accept_ranges`` & ``complete_length`` so video seeking is supported
     response.make_conditional(request, accept_ranges=True, complete_length=media_file.length)
 
-    if filename is None and request.args.get("filename"):
-        filename = request.args.get("filename")
+    return response
 
+
+def set_filename_in_response(response, filename, media_file):
     if filename:
         _filename, ext = os.path.splitext(filename)
         if not ext:
             ext = guess_media_extension(media_file.content_type)
         filename = secure_filename(f"{_filename}{ext}")
-        response.headers["Content-Type"] = media_file.content_type
-        response.headers["Content-Disposition"] = 'attachment; filename="{}"'.format(filename)
+        response.headers["Content-Disposition"] = f'attachment; filename="{filename}"'
     else:
         response.headers["Content-Disposition"] = "inline"
 
+
+@blueprint.route("/assets/<path:media_id>", methods=["GET"])
+def get_upload(media_id):
+    if not get_app_config("PUBLIC_DASHBOARD") and not is_valid_session():
+        return clear_session_and_redirect_to_login()
+
+    media_file = get_media_file(media_id)
+    if not media_file:
+        abort(404)
+
+    response = construct_response(media_file)
+    filename = request.args.get("filename")
+    set_filename_in_response(response, filename, media_file)
     return response
 
 
