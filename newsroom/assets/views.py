a0d3478900b001fda6fd1ad4abce08ed43ab84f3
Mark Pittaway <mark.pittaway@gmail.com>
2024-08-14 17:52:42 +1000

    fix asset tests

--- newsroom/assets/views.py
+++ newsroom/assets/views.py
@@ -7,6 +7,7 @@ from werkzeug.utils import secure_filename
 
 from superdesk.core import get_app_config, get_current_app
 from superdesk.core.web import Response, Request
+from superdesk.flask import request as flask_request
 from superdesk.media.media_operations import guess_media_extension
 
 from .module import assets_endpoints
@@ -36,25 +37,31 @@ async def get_upload(args: RouteArguments, params: UrlParams, request: Request) 
     content = await media_file.read()
 
     app = get_current_app()
+    metadata = media_file.metadata or {}
+    mimetype = metadata.get("contentType", media_file.content_type)
     file_body = app.as_any().response_class.io_body_class(BytesIO(content))
-    response = app.response_class(file_body, mimetype=media_file.content_type)
+    response = app.response_class(file_body, mimetype=mimetype)
     response.content_length = media_file.length
     response.last_modified = media_file.upload_date
-    response.set_etag(media_file.md5)
+
+    # TODO-ASYNC: Set etag from ``media_file`` (as it's curre
+    if getattr(media_file, "md5", None):
+        response.set_etag(media_file.md5)
+
     response.cache_control.max_age = CACHE_MAX_AGE
     response.cache_control.s_max_age = CACHE_MAX_AGE
     response.cache_control.public = True
     response.expires = datetime.now(timezone.utc) + timedelta(seconds=CACHE_MAX_AGE)
 
     # Add ``accept_ranges`` & ``complete_length`` so video seeking is supported
-    await response.make_conditional(request, accept_ranges=True, complete_length=media_file.length)
+    await response.make_conditional(flask_request, accept_ranges=True, complete_length=media_file.length)
 
     if params.filename:
         _filename, ext = path.splitext(params.filename)
         if not ext:
-            ext = guess_media_extension(media_file.content_type)
+            ext = guess_media_extension(mimetype)
         filename = secure_filename(f"{_filename}{ext}")
-        response.headers["Content-Type"] = media_file.content_type
+        response.headers["Content-Type"] = mimetype
         response.headers["Content-Disposition"] = 'attachment; filename="{}"'.format(filename)
     else:
         response.headers["Content-Disposition"] = "inline"
