cd67b3cbd1cfd4e30baf2081316d9195edbca2ed
Helmy Giacoman <helmygb@gmail.com>
2024-08-30 19:06:02 +0200

    Fix `create_user` not setting user password correctly
    Given the command was using old service class, the password was being saved in plain text.
    
    Thanks @devketanpro for finding this.
    
    NHUB-530

--- newsroom/commands/create_user.py
+++ newsroom/commands/create_user.py
@@ -1,6 +1,9 @@
 import click
+from bson import ObjectId
 from quart.cli import with_appcontext
-from superdesk import get_resource_service
+from newsroom.async_utils import run_async_to_sync
+from newsroom.users.model import UserResourceModel
+from newsroom.users.service import UsersService
 
 from newsroom.auth import get_user_by_email
 from .cli import newsroom_cli
@@ -31,15 +34,16 @@ def create_user(email, password, first_name, last_name, is_admin):
         "user_type": "administrator" if is_admin else "public",
         "is_enabled": True,
         "is_approved": True,
+        "id": ObjectId(),
     }
-
+    new_user = UserResourceModel.model_validate(new_user)
     user = get_user_by_email(email)
 
     if user:
-        print("user already exists %s" % str(new_user))
+        print("User already exists %s" % str(new_user))
     else:
-        print("creating user %s" % str(new_user))
-        get_resource_service("users").post([new_user])
-        print("user saved %s" % (new_user))
+        print("Creating user...")
+        run_async_to_sync(UsersService().create([new_user]))
+        print("User created successfully %s" % (new_user.model_dump(by_alias=True, exclude_unset=True)))
 
     return new_user
