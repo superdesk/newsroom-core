37a9a277a47ec63b4e02dcff4c782a60fca71dd9
Helmy Giacoman <helmygb@gmail.com>
2024-09-06 19:34:13 +0200

    Move news_api section filters
    NHUB-534

--- newsroom/news_api/section_filters/__init__.py
+++ newsroom/news_api/section_filters/__init__.py
@@ -1,16 +1,16 @@
 import superdesk
 
-# from newsroom.section_filters import SectionFiltersService, SectionFiltersResource
+from newsroom.section_filters.section_filters import SectionFiltersService, SectionFiltersResource
 
 
+# TODO-ASYNC: remove this once news_api modules are migrate to async
 def init_app(app):
-    # superdesk.register_resource("section_filters", NewsAPISectionFilterResource, SectionFiltersService, _app=app)
-    pass
+    superdesk.register_resource("section_filters", NewsAPISectionFilterResource, SectionFiltersService, _app=app)
 
 
-# class NewsAPISectionFilterResource(SectionFiltersResource):
-#     """
-#     Overload the newsroom section filter resource so we can set it to be an internal resource
-#     """
+class NewsAPISectionFilterResource(SectionFiltersResource):
+    """
+    Overload the newsroom section filter resource so we can set it to be an internal resource
+    """
 
-#     internal_resource = True
+    internal_resource = True
