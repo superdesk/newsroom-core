0cc471b4869aa0cc85ade3c8c59567b3fd114a5b
Helmy Giacoman <helmygb@gmail.com>
2024-09-13 18:11:14 +0200

    Make Navigation name unique
    NHUB-533

--- newsroom/navigations/model.py
+++ newsroom/navigations/model.py
@@ -1,11 +1,11 @@
 from typing import Annotated, Optional
 
-from superdesk.core.resources.validators import validate_iunique_value_async
 from newsroom.core.resources.model import NewshubResourceModel
+from newsroom.core.resources.validators import validate_multi_field_iunique_value_async
 
 
 class Navigation(NewshubResourceModel):
-    name: str
+    name: Annotated[str, validate_multi_field_iunique_value_async("navigations", ["name", "product_type"])]
     description: str = ""
     is_enabled: bool = True
     order: Optional[int] = None
