378cc5b119fd60a2c5e55bcc58d6af72c2f1f507
Petr Jašek <jasekpetr@gmail.com>
2024-09-25 08:51:37 +0200

    use elastic in wire expiry (#1093)
    there were timeouts from mongo
    
    CPCN-849

--- tests/commands/test_remove_expired_items.py
+++ tests/commands/test_remove_expired_items.py
@@ -1,16 +1,37 @@
 from datetime import datetime, timedelta
-from newsroom.commands.remove_expired import remove_expired
-from newsroom.utils import find_one
+from newsroom.utils import query_resource
+from content_api.commands import RemoveExpiredItems
 
 
 def test_remove_expired_items(app):
     items = [
-        {"_id": "expired", "versioncreated": datetime(2020, 10, 1), "expiry": datetime.utcnow() - timedelta(days=1)},
+        {"_id": "expired", "_updated": datetime.utcnow(), "expiry": datetime.utcnow() - timedelta(days=1)},
+        {
+            "_id": "expired2",
+            "_updated": datetime.utcnow() - timedelta(days=1),
+            "expiry": datetime.utcnow() - timedelta(days=1),
+        },
+        {"_id": "expired3", "_updated": datetime.utcnow() - timedelta(days=800)},
+        {"_id": "not-expired", "_updated": datetime.utcnow(), "expiry": datetime.utcnow() + timedelta(days=90)},
+        {
+            "_id": "not-expired2",
+            "_updated": datetime.utcnow() - timedelta(800),
+            "expiry": datetime.utcnow() + timedelta(days=1),
+        },
     ]
 
+    app.data.remove("items")
     app.data.insert("items", items)
 
-    remove_expired(1)
+    assert query_resource("items").count() == len(items)
 
-    expired = find_one("items", _id="expired")
-    assert expired is None
+    app.config.update({"CONTENT_API_EXPIRY_DAYS": 500})
+    RemoveExpiredItems().run()
+
+    cursor = query_resource("items")
+    ids = [doc["_id"] for doc in cursor]
+    assert 2 == cursor.count(), ids
+
+    ids = [doc["_id"] for doc in cursor]
+    assert "not-expired" in ids
+    assert "not-expired2" in ids
