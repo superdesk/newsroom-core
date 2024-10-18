1b3d4cc3dafa4f31837e869a52717ec986cf0af9
Nikola Stojanovic <68916411+dzonidoo@users.noreply.github.com>
2024-10-18 12:25:45 +0200

    As a NH user I like to know which timeframe has been used as a default filter for the wire search (#1116)
    * created popover component and default timeframe informations
    
    * changes after review

--- assets/search/components/SearchResultsBar/SearchResultTagsList.tsx
+++ assets/search/components/SearchResultsBar/SearchResultTagsList.tsx
@@ -8,7 +8,7 @@ import {IFilterGroup, INavigation, ISearchFields, ISearchParams, ITopic, IUser} 
 import {SearchResultTagList} from './SearchResultTagList';
 import {gettext} from 'utils';
 import {getTopicUrl} from 'search/utils';
-import {IDateFilter} from 'interfaces/common';
+import {IDateFilters} from 'interfaces/common';
 
 export interface IProps {
     user: IUser;
@@ -35,7 +35,7 @@ export interface IProps {
 
     saveMyTopic?: (params: ISearchParams) => void;
     deselectMyTopic?: (topicId: ITopic['_id']) => void;
-    dateFilters?: IDateFilter;
+    dateFilters?: IDateFilters;
 }
 
 export function SearchResultTagsList({
