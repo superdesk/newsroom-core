12ccdf0288f17e0424215dbadb4cce9b37e918a8
Mark Pittaway <mark.pittaway@gmail.com>
2024-10-28 16:21:03 +1100

    Move SUPPORTED_NESTED_SEARCH_FIELDS to a module level variable

--- newsroom/agenda/__init__.py
+++ newsroom/agenda/__init__.py
@@ -23,6 +23,9 @@ blueprint = Blueprint("agenda", __name__)
 from . import views  # noqa
 
 
+AGENDA_NESTED_SEARCH_FIELDS = ["subject"]
+
+
 def init_app(app):
     superdesk.register_resource("agenda", AgendaResource, AgendaService, _app=app)
     superdesk.register_resource("agenda_featured", FeaturedResource, FeaturedService, _app=app)
@@ -79,7 +82,7 @@ def init_app(app):
             },
         ]
 
-    init_nested_aggregation(AgendaResource, app.config.get("AGENDA_GROUPS", []), aggregations)
+    init_nested_aggregation("agenda", AGENDA_NESTED_SEARCH_FIELDS, app.config.get("AGENDA_GROUPS", []), aggregations)
 
     if app.config.get("AGENDA_HIDE_COVERAGE_ASSIGNEES"):
         PRIVATE_FIELDS.extend(["*.assigned_desk_*", "*.assigned_user_*"])
