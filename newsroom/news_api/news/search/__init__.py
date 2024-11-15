5366184013b220025c4ee0648bd06aa7e0fe2c12
Helmy Giacoman <helmygb@gmail.com>
2024-11-16 00:58:10 +0100

    Remove not needed modules
    NHUB-572

--- newsroom/news_api/news/search/__init__.py
+++ newsroom/news_api/news/search/__init__.py
@@ -1,13 +1,4 @@
-import superdesk
-
-from .resource import NewsAPISearchResource
-from .service import NewsAPISearchService
-
 from superdesk.core.module import Module
 from .views import news_api_search_endpoints
 
 module = Module(name="newsroom.news_api.search", endpoints=[news_api_search_endpoints])
-
-
-def init_app(app):
-    superdesk.register_resource("news/search1", NewsAPISearchResource, NewsAPISearchService, _app=app)
