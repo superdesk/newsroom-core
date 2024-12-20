dee882f5e450d1e17c7ca635397ed51f040f8d33
Petr Jašek <jasekpetr@gmail.com>
2024-12-20 07:34:59 +0100

    drop support for python 3.8 (#1215)
    * drop support for python 3.8
    
    and update github actions setup
    
    * use mongo 6
    
    * use mongo 6 for e2e

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     scripts=[],
+    python_requires=">=3.9",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
