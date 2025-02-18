726e4c6e751d80a5465ccf4a772ea446681fef9a
devketanpro <devketanpro11@gmail.com>
2025-02-18 15:15:02 +0530

    fix mypy

--- newsroom/mgmt_api/utils.py
+++ newsroom/mgmt_api/utils.py
@@ -1,6 +1,6 @@
 import bson
 import ipaddress
-from typing import List, Dict, Optional, Any
+from typing import List, Optional
 from quart_babel import gettext
 
 from superdesk.core.types import Response
@@ -9,7 +9,7 @@ from newsroom.types import CompanyProduct
 from newsroom.products import ProductsService
 
 
-async def validate_product_refs(product_refs: List[CompanyProduct]) -> List[Dict[Any, Any]]:
+async def validate_product_refs(product_refs: List[CompanyProduct]) -> List[CompanyProduct]:
     products_service = ProductsService()
     product_refs = [ref.to_dict() if isinstance(ref, CompanyProduct) else ref for ref in product_refs]
     product_ids = [bson.ObjectId(ref["_id"]) for ref in product_refs]
@@ -26,7 +26,7 @@ async def validate_product_refs(product_refs: List[CompanyProduct]) -> List[Dict
 
 def get_errors_company(updates: CompanyProduct, original: Optional[CompanyProduct] = None) -> Response:
     if original is None:
-        original = CompanyProduct()  # Create a default empty CompanyProduct object if None
+        original = {}
 
     updates_data = updates.to_dict()
     if not (updates_data.get("name") or original.get("name")):
