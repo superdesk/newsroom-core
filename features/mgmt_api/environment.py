1c85319c26cd7ecc46b9c49ae52fc7e844d3ef4c
Mark Pittaway <mark.pittaway@gmail.com>
2025-03-26 16:20:28 +1100

    [NHUB-621] Implement Quart integration for OAuth2 server

--- features/mgmt_api/environment.py
+++ features/mgmt_api/environment.py
@@ -1,6 +1,6 @@
 import asyncio
 from superdesk.tests.environment import setup_before_all, setup_before_scenario
-from newsroom.auth_server.oauth2 import generate_jwt_token, config_oauth
+from newsroom.auth_server.client import authorization
 from newsroom.mgmt_api.app import get_app as _get_app
 from newsroom.mgmt_api.default_settings import CORE_APPS, MODULES, ASYNC_AUTH_CLASS, URL_PREFIX
 from superdesk.tests import setup as setup_app
@@ -61,10 +61,9 @@ async def before_scenario_async(context, scenario):
     context.app = get_app(config=config)
     context.headers = []
     async with context.app.app_context():
-        config_oauth(context.app)
+        authorization.init_app(context.app, register_endpoint=False)
         await setup_app(context, config, app_factory=get_app, reset=True)
         await setup_before_scenario(context, scenario, config, app_factory=get_app)
-        token = generate_jwt_token(TestClient(), "client_credentials", "test", "")
-        if isinstance(token, bytes):
-            token = token.decode()
+
+        token = authorization.generate_jwt_token(TestClient(), "client_credentials", "test", "")
         context.headers.append(("Authorization", f"Bearer {token}"))
