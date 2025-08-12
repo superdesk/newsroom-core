bc5d29fe739cf2840aa18452183f86d84388afec
Helmy Giacoman <helmygb@gmail.com>
2025-08-12 16:47:23 +0200

    Rename health endpoint group to system_health
    Changed the EndpointGroup name from 'health' to 'system_health' for clarity. Also updated the health endpoint docstring to remove not longer needed information.
    
    NHUB-627

--- newsroom/system/health.py
+++ newsroom/system/health.py
@@ -7,7 +7,7 @@ from superdesk.system.health import get_health_status
 
 
 logger = logging.getLogger(__name__)
-health_endpoints = EndpointGroup("health", __name__, auth=False)
+health_endpoints = EndpointGroup("system_health", __name__, auth=False)
 
 
 @health_endpoints.endpoint("/system/health", methods=["GET"])
@@ -16,7 +16,7 @@ async def health():
     Health check endpoint for the system.
 
     Returns a JSON response containing the application name, the status of various health checks,
-    and an overall system status. Each health check is run and its result is included in the output.
+    and an overall system status.
     """
     app_name = get_app_config("SITE_NAME", "Newshub")
 
