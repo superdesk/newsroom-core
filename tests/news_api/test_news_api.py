a7991bdcbfc5ebac892661fa97ab215851eeeb2f
marwoodandrew <amarwood@aap.com.au>
2025-11-13 12:07:41 +1100

    [SDAN-749] Product endpoint returns error

--- tests/news_api/test_news_api.py
+++ tests/news_api/test_news_api.py
@@ -1,6 +1,6 @@
 from bson import ObjectId
-
-from tests.core.utils import create_entries_for, find_one_for
+from newsroom.types import SectionEnum
+from tests.core.utils import create_entries_for, find_one_for, update_entries_for
 
 
 async def test_news_api_root_links(client, app):
@@ -28,3 +28,80 @@ async def test_news_api_root_links(client, app):
         "account/products": "Account Products Search",
         "account/products/<string:product_id>": "Get Account Product",
     }
+
+
+async def test_product_search(client, app):
+    company_id = ObjectId()
+    product_id = ObjectId()
+    company = await create_entries_for(
+        "companies",
+        [
+            {
+                "_id": company_id,
+                "name": "Test Company",
+                "is_enabled": True,
+                "products": [{"_id": product_id, "section": SectionEnum.NEWS_API}],
+            }
+        ],
+    )
+    await create_entries_for(
+        "products",
+        [
+            {
+                "_id": product_id,
+                "name": "Test Product",
+                "is_enabled": True,
+                "product_type": SectionEnum.NEWS_API,
+                "query": "*",
+            }
+        ],
+    )
+    await create_entries_for("news_api_tokens", [{"company": company_id, "enabled": True}])
+    token = await find_one_for("news_api_tokens", company=company_id)
+    response = await client.get("/api/v1/account/products", headers={"Authorization": token.get("token")})
+    assert response.status_code == 200
+    data = await response.get_json()
+    assert data["_items"][0].get("_id") == str(product_id)
+    response = await client.get(f"/api/v1/account/products/{product_id}", headers={"Authorization": token.get("token")})
+    assert response.status_code == 200
+    data = await response.get_json()
+    assert data.get("_links").get("search").get("href") == f"news/search/?products={product_id}"
+    assert data.get("_links").get("feed").get("href") == f"news/feed/?products={product_id}"
+    response = await client.get(
+        f"/api/v1/news/search/?products={product_id}", headers={"Authorization": token.get("token")}
+    )
+    assert response.status_code == 200
+    bad_product = ObjectId()
+    response = await client.get(
+        f"/api/v1/news/search/?products={bad_product}", headers={"Authorization": token.get("token")}
+    )
+    assert response.status_code == 400
+
+    wire_product = ObjectId()
+    await update_entries_for(
+        "companies",
+        company_id,
+        {
+            "products": [
+                {"_id": product_id, "section": SectionEnum.NEWS_API},
+                {"_id": wire_product, "section": SectionEnum.WIRE},
+            ]
+        },
+        company,
+    )
+    await create_entries_for(
+        "products",
+        [
+            {
+                "_id": wire_product,
+                "name": "Test Product",
+                "is_enabled": True,
+                "product_type": SectionEnum.WIRE,
+                "query": "*",
+            }
+        ],
+    )
+    response = await client.get(
+        f"/api/v1/news/search/?products={wire_product}", headers={"Authorization": token.get("token")}
+    )
+    assert response.status_code == 400
