83d177205bb82b60c0a71b7b4c5ef1080c96b548
Petr Jašek <jasekpetr@gmail.com>
2025-12-17 10:01:12 +0100

    update pytest (#1423)

--- newsroom/tests/conftest.py
+++ newsroom/tests/conftest.py
@@ -1,10 +1,6 @@
 import os
 from pathlib import Path
 from pytest import fixture
-import asyncio
-import contextvars
-import traceback
-import functools
 
 from bson import ObjectId
 from quart import Quart, Config
@@ -18,68 +14,6 @@ from newsroom.limiter import limiter
 from .db import reset_elastic, drop_mongo
 
 root = (Path(__file__).parent / "..").resolve()
-
-
-class Task311(asyncio.tasks.Task):
-    """
-    This is backport of Task from CPython 3.11
-    It's needed to allow context passing
-    """
-
-    def __init__(self, coro, *, loop=None, name=None, context=None):
-        super(asyncio.tasks.Task, self).__init__(loop=loop)
-        if self._source_traceback:
-            del self._source_traceback[-1]
-        if not asyncio.coroutines.iscoroutine(coro):
-            # raise after Future.__init__(), attrs are required for __del__
-            # prevent logging for pending task in __del__
-            self._log_destroy_pending = False
-            raise TypeError(f"a coroutine was expected, got {coro!r}")
-
-        if name is None:
-            self._name = f"Task-{asyncio.tasks._task_name_counter()}"
-        else:
-            self._name = str(name)
-
-        self._num_cancels_requested = 0
-        self._must_cancel = False
-        self._fut_waiter = None
-        self._coro = coro
-        if context is None:
-            self._context = contextvars.copy_context()
-        else:
-            self._context = context
-
-        self._loop.call_soon(self._Task__step, context=self._context)
-        asyncio.tasks._register_task(self)
-
-
-class CustomEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
-    def set_event_loop(self, loop):
-        if loop is not None:
-            context = contextvars.copy_context()
-            loop.set_task_factory(functools.partial(task_factory, context=context))
-        super().set_event_loop(loop)
-
-
-def task_factory(loop, coro, context=None):
-    stack = traceback.extract_stack()
-    for frame in stack[-2::-1]:
-        package_name = Path(frame.filename).parts[-2]
-        if package_name != "asyncio":
-            if package_name == "pytest_asyncio":
-                # This function was called from pytest_asyncio, use shared context
-                break
-            else:
-                # This function was called from somewhere else, create context copy
-                context = None
-            break
-    return Task311(coro, loop=loop, context=context)
-
-
-@fixture(scope="session")
-def event_loop_policy(request):
-    return CustomEventLoopPolicy()
 
 
 def use_config_file(file_path: str):
