9765c0bc0e7671e6a297b2716ab966d0b2f7c311
Petr Jasek <jasekpetr@gmail.com>
2026-03-16 15:40:08 +0100

    fix RuntimeError in when sending notifications
    when the lock is already taken there is runtime error:
    
    RuntimeError
    generator didn't yield

--- newsroom/push/tasks.py
+++ newsroom/push/tasks.py
@@ -1,6 +1,5 @@
 import logging
 
-from contextlib import contextmanager
 from superdesk.lock import lock, unlock
 
 from newsroom.celery_app import celery
@@ -13,33 +12,33 @@ logger = logging.getLogger(__name__)
 notifier = NotificationManager()
 
 
-@contextmanager
-def locked(_id: str, service: str):
-    lock_name = f"notify-{service}-{_id}"
-    if not lock(lock_name, expire=300):
-        logger.debug(f"Lock conflict on {lock_name}")
-        return
+def get_lock_name(service: str, _id: str) -> str:
+    return f"notify-{service}-{_id}"
 
-    logger.debug("Starting task %s", lock_name)
+
+@celery.task
+async def notify_new_wire_item(_id: str, check_topics=True) -> None:
+    lock_name = get_lock_name("wire", _id)
+    if not lock(lock_name, expire=300):
+        logger.debug("Lock conflict on %s", lock_name)
+        return
     try:
-        yield lock_name
+        logger.info("Send notifications for wire item %s", _id)
+        item = await WireSearchServiceAsync().service.find_by_id(_id)
+        if item:
+            await notifier.notify_new_item(item.to_dict(), check_topics=check_topics)
     finally:
         unlock(lock_name)
         logger.debug("Done with %s", lock_name)
 
 
 @celery.task
-async def notify_new_wire_item(_id, check_topics=True):
-    with locked(_id, "wire"):
-        logger.info("Send notifications for wire item %s", _id)
-        item = await WireSearchServiceAsync().service.find_by_id(_id)
-        if item:
-            await notifier.notify_new_item(item.to_dict(), check_topics=check_topics)
-
-
-@celery.task
-async def notify_new_agenda_item(_id, check_topics=True, is_new=False):
-    with locked(_id, "agenda"):
+async def notify_new_agenda_item(_id: str, check_topics=True, is_new=False) -> None:
+    lock_name = get_lock_name("agenda", _id)
+    if not lock(lock_name, expire=300):
+        logger.debug("Lock conflict on %s", lock_name)
+        return
+    try:
         logger.info("Send notifications for agenda item %s", _id)
         service = AgendaItemService()
         agenda = await service.find_by_id(_id)
@@ -52,5 +51,8 @@ async def notify_new_agenda_item(_id, check_topics=True, is_new=False):
             return
 
         agenda_dict = agenda.to_dict()
-        await AgendaItemService().enhance_item(agenda_dict)
+        await service.enhance_item(agenda_dict)
         await notifier.notify_new_item(agenda_dict, check_topics=check_topics)
+    finally:
+        unlock(lock_name)
+        logger.debug("Done with %s", lock_name)
