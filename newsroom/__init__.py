59a7fb8674a40403ba74c73a7a6a03fa11778c79
Petr Jasek <jasekpetr@gmail.com>
2026-08-06 10:38:16 +0200

    release 3.1

--- newsroom/__init__.py
+++ newsroom/__init__.py
@@ -12,8 +12,6 @@ from typing import Dict, List, Tuple
 
 from newsroom.types import UserRole
 
-__version__ = "3.1.0-dev"
-
 # reuse content api dbs
 MONGO_PREFIX = "CONTENTAPI_MONGO"
 ELASTIC_PREFIX = "CONTENTAPI_ELASTICSEARCH"
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(requirements_txt_path, "r") as r:
 
 setup(
     name="Newsroom-Core",
-    version="3.1.0-dev",
+    version="3.1.0",
     description="Newsroom Core library",
     author="Sourcefabric",
     url="https://github.com/superdesk/newsroom-core",
