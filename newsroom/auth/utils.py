58825e46a38709d37720551e8d0e464e11fc3073
Petr Jasek <jasekpetr@gmail.com>
2026-09-02 13:45:12 +0200

    log reason why firebase reset password didn't send email
    CPCN-1207

--- newsroom/auth/utils.py
+++ newsroom/auth/utils.py
@@ -217,10 +217,10 @@ def mask_email_for_logs(email: str | None) -> str:
     if not local_part:
         return "unknown"
 
-    if len(local_part) <= 2:
+    if len(local_part) <= 3:
         masked_local = f"{local_part[:1]}*"
     else:
-        masked_local = f"{local_part[:2]}***{local_part[-1]}"
+        masked_local = f"{local_part[:3]}***{local_part[-2:]}"
 
     return f"{masked_local}@{domain}" if domain else masked_local
 
