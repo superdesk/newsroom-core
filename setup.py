46a32896a58a432b788a2393f1b08a497e484800
Mark Pittaway <mark.pittaway@gmail.com>
2026-09-24 14:38:27 +1000

    Release 3.2.0-beta.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(requirements_txt_path, "r") as r:
 
 setup(
     name="Newsroom-Core",
-    version="3.1.0",
+    version="3.2.0-beta.2",
     description="Newsroom Core library",
     author="Sourcefabric",
     url="https://github.com/superdesk/newsroom-core",
